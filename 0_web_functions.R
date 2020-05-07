1da3c0c7d9e3c200590feb218768b5345051dd51
andybue <56594271+andybue@users.noreply.github.com>
2020-05-07 18:42:38 +0200

    json output for audit charts

--- 0_web_functions.R
+++ 0_web_functions.R
@@ -80,3 +80,138 @@ website_text <- function(audit_file){
   write(text,paste0(proc_input_path,"/Websitetext.txt"))
 }
 
+
+export_audit_information_jsons <-function(audit_file_ = audit_file, portfolio_total_ = portfolio_total,  folder_path = paste0(proc_input_path), project_name_=NA){
+  
+  # Check format
+  assertthat::is.string(folder_path)
+  assertthat::are_equal(is.data.frame(audit_file_),TRUE)
+  
+  if ("Meta Investor" %in% audit_file_$investor_name){
+    audit_file_ <- subset(audit_file_, investor_name != "Meta Investor")
+  }
+  if ("Meta Investor" %in% portfolio_total_$investor_name){
+    portfolio_total_ <- subset(portfolio_total_, investor_name != "Meta Investor")
+  }
+  
+  
+  
+  folder_path <- paste0(folder_path, "/")
+  if(!is.na(project_name_)){folder_path <- paste0(folder_path, project_name_, "_")}
+  # function 
+  export_audit_graph_json(audit_file_, paste0(folder_path, "coveragegraph"))
+  export_audit_invalid_json(portfolio_total_, paste0(folder_path,"invalidsecurities"))
+  export_audit_textvar_json(portfolio_total_, paste0(folder_path,"coveragetextvar"))
+  
+  
+}
+
+export_audit_graph_json <-function(audit_file__, export_path_full){
+  
+
+  audit_file__ <-audit_file__ %>% select("isin", "holding_id","flag")
+  
+  all_flags<- c("Missing currency information","Negative or missing input value","Invalid or missing ISIN","Holding not in Bloomberg database","Included in analysis")
+  flags_in_auditfile <- unique(audit_file__$flag)
+  
+  
+  indicator <- TRUE
+  for (i in 1:length(flags_in_auditfile)){indicator = indicator & (flags_in_auditfile[i] %in% all_flags)} 
+  assertthat::are_equal(indicator, TRUE)
+  
+  
+  number_of_isin  <- length(audit_file__$holding_id)
+
+  missing_currency <- audit_file__ %>% subset(flag == "Missing currency information")
+  number_missing_currency <- length(missing_currency$holding_id)
+  
+  negative_missing_input <- audit_file__ %>% subset(flag == "Negative or missing input value")
+  number_negative_missing_input <- length(negative_missing_input$holding_id)
+  
+  invalid_input   <- audit_file__ %>% subset(flag == "Invalid or missing ISIN")
+  number_invalid_input  <- length(invalid_input$holding_id)
+  
+  not_in_bloomberg <-  audit_file__ %>% subset(flag == "Holding not in Bloomberg database") 
+  number_not_in_bloomberg <- length(not_in_bloomberg$holding_id)
+  
+  included_in_analysis <-  audit_file__ %>% subset(flag == "Included in analysis") 
+  number_included_in_analysis <- length(included_in_analysis$holding_id)
+  
+  all_flags_numbers <- c(number_missing_currency,  number_negative_missing_input,  number_invalid_input,  number_not_in_bloomberg,  number_included_in_analysis)
+  assertthat::are_equal(number_of_isin, sum(all_flags_numbers))
+  
+  number_all_invalid_input <- sum(c(number_missing_currency, number_negative_missing_input,number_invalid_input))
+    
+  legend <- c( "\"Invalid input\"", "\"No data coverage\"", "\"Included in analysis\"")
+  keys <- c("\"key_1\"", "\"key_2\"", "\"key_3\"")
+  values <- c(number_all_invalid_input, number_not_in_bloomberg, number_included_in_analysis)
+  
+  assertthat::are_equal(length(legend), length(keys))
+  assertthat::are_equal(length(values), length(keys))
+  
+  json_head = "{"
+  json_tail = "}"
+  
+  chart_information <- json_head
+  chart_legend <- json_head
+  for (i in 1:(length(keys)-1)){
+    chart_information <- paste0(chart_information," " , keys[i],": ", values[i],",")
+    chart_legend <- paste0(chart_legend," " , keys[i],": ", legend[i],",")
+    } 
+  i = length(keys)
+  chart_information <- paste0(chart_information," " , keys[i],": ", values[i])
+  chart_legend <- paste0(chart_legend," " , keys[i],": ", legend[i])
+  
+  chart_information <- paste0(chart_information, json_tail)
+  chart_legend <- paste0(chart_legend, json_tail)
+  
+  write(chart_legend, file=paste0(export_path_full,"legend.json"))
+  write(chart_information, file=paste0(export_path_full,".json"))
+}
+
+export_audit_invalid_json <-function(portfolio_total_,export_path_full){
+  portfolio_total_ <- portfolio_total_ %>% subset(flag %in% c("Missing currency information", "Negative or missing input value","Invalid or missing ISIN"))
+  portfolio_total_ <- portfolio_total_ %>% select("isin", "market_value", "currency", "flag")
+  portfolio_total_ <- portfolio_total_[order(-portfolio_total_$market_value),]
+  
+  colnames(portfolio_total_) <- c("isin","marketValues","currency","flag")
+  invalidsecurties <- toJSON(portfolio_total_, dataframe = c('columns'))
+  write(invalidsecurties, file=paste0(export_path_full,".json"))
+} 
+export_audit_textvar_json <-function(portfolio_total_, export_path_full){
+  portfolio_total_ <- portfolio_total_ %>% select("value_usd", "asset_type", "has_valid_input")
+  total_v <- sum(portfolio_total_$value_usd)
+  included <- portfolio_total_ %>% subset(has_valid_input == TRUE)
+  included_v <- sum(included$value_usd)
+  bonds <- portfolio_total_ %>% subset(asset_type == "Bonds")
+  bonds_v <- sum(bonds$value_usd)
+  equity <- portfolio_total_ %>% subset(asset_type == "Equity")
+  equity_v <- sum(equity$value_usd)
+  
+  assertthat::are_equal(included_v<=total_v, TRUE)
+  assertthat::are_equal(bonds_v+equity_v-included_v<1, TRUE)
+  
+  total_v <- round(total_v)
+  total_v <- as.character(total_v)
+  included_v <- round(included_v)
+  bonds_v <- round(bonds_v)
+  equity_v <- round(equity_v)
+  
+  keys <- c("\"total\"", "\"included\"", "\"bonds\"", "\"equity\"")
+  values <- c(total_v,included_v,bonds_v,equity_v)
+  
+  
+  json_head = "{"
+  json_tail = "}"
+  
+  text_varibles <- json_head
+  for (i in 1:(length(keys)-1)){
+    text_varibles <- paste0(text_varibles," " , keys[i],": ", values[i],",")
+    } 
+  i = length(keys)
+  text_varibles <- paste0(text_varibles," " , keys[i],": ", values[i])
+ 
+  text_varibles <- paste0(text_varibles, json_tail)
+ 
+  write(text_varibles, file=paste0(export_path_full,".json"))
+  }
