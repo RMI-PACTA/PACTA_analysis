8351776f18da96beb2343fb0fdad43c788fc8b16
Clare2D <clare@2degrees-investing.org>
2020-06-24 17:10:59 +0200

    Added ungroup

--- 2_project_input_analysis.R
+++ 2_project_input_analysis.R
@@ -48,8 +48,6 @@ portfolio <- add_revenue_split(has_revenue, portfolio, revenue_data)
 
 portfolio <- create_ald_flag(portfolio, comp_fin_data, debt_fin_data)
 
-
-
 eq_portfolio <- create_portfolio_subset(portfolio, 
                                         "Equity", 
                                         comp_fin_data)
