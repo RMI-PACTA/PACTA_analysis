53c4ac1606410e8f0e64fdd7123fb322982bcc03
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-07-28 12:41:09 +0200

    align portfolio_test

--- 0_portfolio_test.R
+++ 0_portfolio_test.R
@@ -94,6 +94,14 @@ calculate_port_weight <- function(portfolio, grouping_variables){
     mutate(port_total_aum = sum(value_usd, na.rm =  T), 
            port_weight = value_usd/port_total_aum)
   
+  temp <- portfolio %>% 
+    group_by(!!!rlang::syms(grouping_variables)) %>% 
+    mutate(total_port_weight = sum(port_weight))
+  
+  total_port_weight_per_portfolio <- signif(unique(temp$total_port_weight),2)
+  # check that all portfolio port_weight's sum to 1
+  if (!all(total_port_weight_per_portfolio == 1.0)) {stop("Port weight calculation error")}
+  
   portfolio  
   
 }
@@ -222,7 +230,7 @@ calculate_weights <- function(portfolio, portfolio_type, grouping_variables){
     rename(has_ald = has_ald_in_fin_sector)
   
   port_sub <- portfolio %>% 
-    select(grouping_variables, holding_id,id, id_name, company_name, value_usd, number_of_shares, 
+    select(all_of(grouping_variables), holding_id,id, id_name, company_name, value_usd, number_of_shares, 
            current_shares_outstanding_all_classes, financial_sector, has_ald)
   
   port_sub <- calculate_port_weight(port_sub, grouping_variables)
@@ -251,7 +259,7 @@ merge_in_ald <- function(portfolio, ald_scen){
 
 port_weight_allocation <- function(port_ald, portfolio_type){
   
-  port_ald_pw <- port_ald %>% filter(has_ald == TRUE)
+  port_ald_pw <- port_ald %>% filter(has_ald == TRUE, financial_sector == ald_sector)
   
   if(data_check(port_ald_pw)){
     
@@ -314,23 +322,102 @@ merge_in_geography <- function(portfolio, ald_raw, sectors_for_maps){
   
 }
 
+calculate_scenario_alignment <- function(df){	
+  
+  browntechs <- c("Oil","Gas","Coal","CoalCap","GasCap", "OilCap","ICE")	
+  
+  df$trajectory_deviation <- (df$plan_alloc_wt_tech_prod - df$scen_alloc_wt_tech_prod) / df$scen_alloc_wt_tech_prod	
+  df$trajectory_deviation <- ifelse(df$scen_alloc_wt_tech_prod == 0, ifelse(df$plan_alloc_wt_tech_prod == 0, 0, -1), df$trajectory_deviation)	
+  
+  df$trajectory_alignment <-  ifelse(!df$technology %in% browntechs, 1 * df$trajectory_deviation, -1 * df$trajectory_deviation)	
+  
+  df	
+}	
 
-calculate_scenario_alignment <- function(df){
-  
-  browntechs <- c("Oil","Gas","Coal","CoalCap","GasCap","ICE")
-  
-  df$trajectory_deviation <- (df$plan_alloc_wt_tech_prod - df$scen_alloc_wt_tech_prod) / df$scen_alloc_wt_tech_prod
-  df$trajectory_deviation <- ifelse(df$scen_alloc_wt_tech_prod == 0, ifelse(df$plan_alloc_wt_tech_prod == 0, 0, -1), df$trajectory_deviation)
-  
-  df$trajectory_alignment <-  ifelse(!df$technology %in% browntechs, 1 * df$trajectory_deviation, -1 * df$trajectory_deviation)
-  
-  df
+calculate_technology_share <- function(df){	
+  df <- df %>%	
+    ungroup() %>%	
+    mutate(plan_tech_share = plan_alloc_wt_tech_prod/plan_alloc_wt_sec_prod,	
+           scen_tech_share = scen_alloc_wt_tech_prod/scen_alloc_wt_sec_prod)	
+  df	
 }
 
-calculate_technology_share <- function(df){
-  df <- df %>%
-    ungroup() %>%
-    mutate(plan_tech_share = plan_alloc_wt_tech_prod/plan_alloc_wt_sec_prod,
-           scen_tech_share = scen_alloc_wt_tech_prod/scen_alloc_wt_sec_prod)
-  df
+gather_and_save_project_results <- function(
+  results_folder_path = results_path, 
+  aggregation_level = "portfolio", 
+  portfolios_per_file = 500, 
+  year_filter = NA, 
+  allocation_filter = NA){
+  
+  k <- 1	
+  j <- 1	
+  
+  for (i in 1:length(all_investors)){	
+    
+    investor_name_select <- all_investors[i]	
+    print(investor_name_select)	
+    
+    results_path_investor <- paste0(results_path,"/",investor_name_select,"/")	
+    
+    if(file.exists(paste0(results_path_investor,"/Equity_results_",aggregation_level,".rda"))){	
+      
+      results_eq <- as.data.frame(read_rds(paste0(results_path_investor,"/Equity_results_",aggregation_level,".rda")))	
+      
+      if(typeof(year_filter) %in% c("integer","double")){	
+        results_eq <- results_eq %>% filter(year %in% year_filter)	
+      }	
+      
+      if(typeof(allocation_filter) %in% c("character")){	
+        results_eq <- results_eq %>% filter(allocation %in% allocation_filter)	
+      }	
+      
+      if(nrow(results_eq)!=0){	
+        if (exists("all_results_eq")){	
+          all_results_eq <- rbind(all_results_eq, results_eq)	
+        }else{	
+          all_results_eq <- results_eq	
+        }}	
+      
+    }	
+    
+    if(file.exists(paste0(results_path_investor,"Bonds_results_",aggregation_level,".rda"))){	
+      
+      results_cb <- read_rds(paste0(results_path_investor,"Bonds_results_",aggregation_level,".rda"))	
+      
+      if(typeof(year_filter) %in% c("integer","double")){	
+        results_cb <- results_cb %>% filter(year %in% year_filter)	
+      }	
+      
+      if(typeof(allocation_filter) %in% c("character")){	
+        results_cb <- results_cb %>% filter(allocation %in% allocation_filter)	
+      }	
+      
+      if (exists("all_results_cb")){	
+        all_results_cb <- rbind(all_results_cb, results_cb)	
+      }else{	
+        all_results_cb <- results_cb	
+      }	
+    }	
+    
+    
+    
+    if (j==portfolios_per_file){	
+      if(exists("all_results_cb")){	
+        saveRDS(all_results_cb,paste0(results_path,"/Bonds_results_",aggregation_level,"_",k,".rda"))	
+        rm(all_results_cb)}	
+      if(exists("all_results_eq")){	
+        saveRDS(all_results_eq,paste0(results_path,"/Equity_results_",aggregation_level,"_",k,".rda"))	
+        rm(all_results_eq)}	
+      j = 1	
+      k = k + 1	
+    }else{	
+      j = j + 1	
+    }	
+  }	
+  
+  if(exists("all_results_cb")){	
+    saveRDS(all_results_cb,paste0(results_path,"/Bonds_results_",aggregation_level,".rda"))}	
+  if(exists("all_results_eq")){	
+    saveRDS(all_results_eq,paste0(results_path,"/Equity_results_",aggregation_level,".rda"))}	
+  
 }
