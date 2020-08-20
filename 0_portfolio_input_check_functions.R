fe4448ff1da041f3b9476d0bf88181a794d0ed4b
jacobvjk <jacob.kastl@gmail.com>
2020-08-20 16:46:32 +0200

    removed surplus / from the funds_data path

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -847,16 +847,18 @@ get_and_clean_fund_data <- function(){
   
   fund_data <- NA
   # Fund Data
-  if(file.exists(paste0(analysis_inputs_path,"/fund_data_",financial_timestamp,".rda"))){
-    fund_data <- readRDS(paste0(analysis_inputs_path,"/fund_data_",financial_timestamp,".rda"))
-  }else if(file.exists(paste0(analysis_inputs_path,"/fund_data_2018Q4.rda"))){
-    fund_data <- readRDS(paste0(analysis_inputs_path,"/fund_data_2018Q4.rda"))
+  if(file.exists(paste0(analysis_inputs_path,"fund_data_",financial_timestamp,".rda"))){
+    fund_data <- readRDS(paste0(analysis_inputs_path,"fund_data_",financial_timestamp,".rda"))
+  } else if(file.exists(paste0(analysis_inputs_path,"fund_data_",financial_timestamp,".rds"))){
+    fund_data <- readRDS(paste0(analysis_inputs_path,"fund_data_",financial_timestamp,".rds"))
+  } else if(file.exists(paste0(analysis_inputs_path,"fund_data_2018Q4.rda"))){
+    fund_data <- readRDS(paste0(analysis_inputs_path,"fund_data_2018Q4.rda"))
     print("Old Fund Data being used. Replace FundsData2018Q4 or check name of file.")
   # }else if(file.exists(paste0(analysis_inputs_path, "/SFC_26052020_funds.csv"))){
   #   fund_data <- read_csv(paste0(analysis_inputs_path, "/SFC_26052020_funds.csv"))
   #   print("2020Q2 SFC fund data being used")
-  }else if(file.exists(paste0(analysis_inputs_path, "/Liechtenstein_p2020_fund_holdings_2019Q4.csv"))){
-    fund_data <- read_csv(paste0(analysis_inputs_path, "/Liechtenstein_p2020_fund_holdings_2019Q4.csv"))
+  }else if(file.exists(paste0(analysis_inputs_path, "Liechtenstein_p2020_fund_holdings_2019Q4.csv"))){
+    fund_data <- read_csv(paste0(analysis_inputs_path, "Liechtenstein_p2020_fund_holdings_2019Q4.csv"))
     print("2019Q4 (?) Liechtenstein fund data being used")
   }else{
     if(!data_check(fund_data)){
