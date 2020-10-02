bbbe1d6bcc122dd2a60c479fe15c31a644d8b5ed
Mauro Lepore <maurolepore@gmail.com>
2020-10-01 21:30:26 -0500

    Tweak docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # docker image import https://tool.transitionmonitor.com/downloads/2diirunner_1.3.0.tar.xz 2diirunner:1.3.0
 
 # TODO: Change directory to PACTA_analysis/
@@ -5,22 +7,30 @@ analysis=$(pwd)
 data=$(dirname $(pwd))/pacta-data
 report=$(dirname $(pwd))/create_interactive_report
 
+# To commit changes to this image, remove the flag --rm
 docker run --rm -ti \
   --mount type=bind,source=$analysis,target=/bound \
   --mount type=bind,source=$data,target=/pacta-data \
   --mount type=bind,source=$report,target=/create_interactive_report \
   2diirunner:1.3.0 /bin/bash
 
+# Setup bash profile
+# Nicer prompt
+PS1='docker:\w$ '
+echo "PS1='docker:\w$ '" >> ~/.profile
 # Set permissions so that new files can be deleted/overwritten outside docker
 umask 000
-# Nicer prompt
-PS1="docker:\w$ "
+echo "umask 000" >> ~/.profile
 
 cd /bound
 
 # Install faster, using binaries for linux from RStudio package manager
 echo 'options(repos = c(CRAN = "https://packagemanager.rstudio.com/all/__linux__/bionic/latest"))' >> ~/.Rprofile
+Rscript -e "update.packages(ask = FALSE)"
 Rscript -e "install.packages(c('here','writexl','countrycode', 'renv'))"
+
+# TODO: We can exit then commit the changes to this container up to this point
+# docker commit -a "Mauro Lepore" <container> -m "Update and install packages"
 
 Rscript --vanilla web_tool_script_1.R TestPortfolio_Input
 Rscript --vanilla web_tool_script_2.R TestPortfolio_Input
