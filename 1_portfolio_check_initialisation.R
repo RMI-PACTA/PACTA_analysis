3fdbd94bcbd144184d99ce3f5c00d0e4b843904c
Mauro Lepore <maurolepore@gmail.com>
2020-10-06 15:01:25 -0500

    Revert "Merge branch 'master' into current_web_functionality"
    This reverts commit acdedabadccdcdeb05cc2e0384f9c78e77a5abd6.
    
    This takes the state of this branch back to before merging it with
    master.

--- 1_portfolio_check_initialisation.R
+++ 1_portfolio_check_initialisation.R
@@ -30,26 +30,16 @@ source("0_portfolio_input_check_functions.R")
 source("0_global_functions.R")
 source("0_sda_approach.R")
 
+project_name <- "BondsTest"
+twodii_internal <- TRUE
+# TRUE or FALSE: TRUE means that the code is running on a 2dii laptop with dropbox connection
 
-# Set Project Settings
-
-# within the "project_setting.yml" config file, set the project_name, the twodii_internal switch,
-# and the external data locations, if necessary.
-# the project_name will determine the name of the folder that is created for this project
-# Set twodii_internal to TRUE to run the analysis on an internal 2dii laptop
-# This setting uses the dropbox connection for data import
-# Set twodii_internal to FALSE, tu use external data locations
-# Specify these data locations in the config file "project_settings.yml" in the repo
-
-cfg <- config::get(file = "project_settings.yml")
-
-check_valid_cfg(cfg)
-
-project_name <- cfg$project_name
-twodii_internal <- cfg$project_internal$twodii_internal
-project_location_ext <- cfg$project_internal$project_location_ext
-data_location_ext <- cfg$project_internal$data_location_ext
-
+#####################################################################
+###ONLY FOR EXTERNAL PROJECTS (twodii_internal <- FALSE):
+# Variables must exist for internal projects
+project_location_ext <- "C:/Users/clare/Desktop/ExternalTest"
+data_location_ext <- "C:/Users/clare/Desktop/Git/pacta-data/2019Q4"
+#####################################################################
 
 create_project_folder(project_name, twodii_internal, project_location_ext)
 
