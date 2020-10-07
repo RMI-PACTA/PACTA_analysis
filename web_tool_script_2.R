6b766a7ecffd84f7898e9a42a81af4520c075afe
jacobvjk <jacob.kastl@gmail.com>
2020-10-07 17:59:16 +0200

    use fs::path and correct DS_Store in .gitignore

--- web_tool_script_2.R
+++ web_tool_script_2.R
@@ -197,6 +197,6 @@ if (file.exists(bonds_inputs_file)){
   }
 }
 
-invisible(set_portfolio_parameters(file_path = paste0(par_file_path,"/",portfolio_name_ref_all,"_PortfolioParameters.yml")))
+invisible(set_portfolio_parameters(file_path = fs::path(par_file_path,paste0(portfolio_name_ref_all,"_PortfolioParameters.yml"))))
 
 source(paste0(stress_test_path, "web_tool_stress_test.R"))
