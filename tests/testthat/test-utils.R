6e981d2e59e90179a361a6cde0b30774e68884ae
Mauro Lepore <maurolepore@gmail.com>
2020-10-09 07:53:48 -0500

    Polish test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,12 +7,11 @@ test_that("use_r_packages() puts listed packages on search() path", {
 
   is_attached <- function(package) package %in% .packages()
 
-  if (!is_attached(package)) {
+  if (is_attached(package)) {
+    stop("This test is useful only if `package` is not in .packages().")
+  } else {
     use_r_packages(path)
     on.exit(detach(paste0("package:", package), character.only = TRUE))
-  } else {
-    stop("This test is wrong.")
+    expect_true(is_attached(package))
   }
-
-  expect_true(is_attached(package))
 })
