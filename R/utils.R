bf7c030e390ecbf4a2dab2b36377e0973e108086
Mauro Lepore <maurolepore@gmail.com>
2020-10-10 14:47:49 -0500

    New docker/ extends base docker image

--- R/utils.R
+++ R/utils.R
@@ -13,6 +13,42 @@ setup_project <- function() {
   source(path)
 }
 
-use_r_packages <- function(path = "deduplicate/load-and-attach-r-packages.R") {
+use_r_packages <- function(path = packages_path()) {
   source(path)
 }
+
+packages_path <- function() {
+  "deduplicate/load-and-attach-r-packages.R"
+}
+
+create_install_calls_for_dockerfile <- function(path = packages_path()) {
+  pkg <- sub("library\\((.*)\\)", "\\1", readLines(path, encoding = "UTF-8"))
+  glue('    && Rscript -e "install.packages(\'{pkg}\')" \\')
+}
+
+update_dockerfile_packages <- function(path = "docker/Dockerfile") {
+  dkr <- readLines(path, encoding = "UTF-8")
+
+  raw <- readLines(packages_path(), encoding = "UTF-8")
+  pkg <- sub("library\\((.*)\\)", "\\1", raw)
+  pkg <- glue::glue('    && Rscript -e "install.packages(\'{pkg}\')" \\')
+  # pkg <- sprintf('    && Rscript -e "install.packages(\'%s\')" \\', pkg)
+
+  out <- c(
+    dkr[1:marker_start(dkr)],
+    pkg,
+    dkr[marker_end(dkr):length(dkr)]
+  )
+  writeLines(out, path)
+
+  invisible(path)
+}
+
+marker_start <- function(lines) {
+  grep("# update-dockerfile-packages-start", lines)
+}
+
+marker_end <- function(lines) {
+  grep("# update-dockerfile-packages-end", lines)
+}
+
---
+++ docker/README.md
@@ -0,0 +1,16 @@
+## Running the web tool with maurolepore/r2diirunner-with-packages
+
+Ensure your working directory is PACTA_analysis, then run one of these workflows:
+
+* Run the web tool in a single step
+
+```bash
+./docker/run-web-tool
+```
+
+* Run the web tool in two steps:
+
+```bash
+./docker/start-container
+/bound/docker/run-r-scripts
+```
