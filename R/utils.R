c8ad93bb8d6e8f9ac11ed5cf513037b052b7b6b3
Mauro Lepore <maurolepore@gmail.com>
2020-10-12 08:50:40 -0500

    Call Rscript only once

--- R/utils.R
+++ R/utils.R
@@ -52,7 +52,18 @@ dockerfile_tail <- function(dockerfile = read_dockerfile()) {
 dockerfile_packages <- function(path = packages_path()) {
   raw <- readLines(path, encoding = "UTF-8")
   pkg <- sub("library\\((.*)\\)", "\\1", raw)
-  glue('    && Rscript -e "install.packages(\'{pkg}\')" \\')
+
+  c(
+    '    && Rscript -e "install.packages(',
+    paste0('             ', format_as_vector(pkg)),
+    '           )'
+  )
+}
+
+format_as_vector <- function(string) {
+  x <- glue('"{string}",')
+  x[length(x)] <- sub(",$", "", x[length(x)])
+  c('c(', glue("  {x}"), ')' )
 }
 
 packages_path <- function() {
