e1cb6a77fdeeace466aeb920acbd3f3a6467b5b1
Mauro Lepore <maurolepore@gmail.com>
2020-10-12 16:18:52 -0500

    Use simpler check_crucial_names()

--- R/check_crucial_names.R
+++ R/check_crucial_names.R
@@ -1,50 +1,17 @@
 #' Check if a named object contains expected names
 #'
-#' It is based on:
-#' <https://www.rdocumentation.org/packages/fgeo.tool/versions/1.2.5/topics/check_crucial_names>.
+#' Based on fgeo.tool::check_crucial_names()
 #'
 #' @param x A named object.
 #' @param expected_names String; expected names of `x`.
 #'
 #' @return Invisible `x`, or an error with informative message.
-#' @export
-#'
-#' @family miscellaneous utility functions
 #'
 #' @examples
 #' x <- c(a = 1)
 #' check_crucial_names(x, "a")
 #' try(check_crucial_names(x, "bad"))
-#'
-#' data <- data.frame(a = 1)
-#' check_crucial_names(data, "a")
-#' try(check_crucial_names(data, "bad"))
-#'
-#' # Applications for the error class "missing_names" ---------------------
-#'
-#' tryCatch(
-#'   check_crucial_names(x, "bad"),
-#'   error = function(e) class(e)
-#' )
-#'
-#' # Wrapping in try() to allow running examples with no failure
-#' try(
-#'   # What's interesting is this
-#'   tryCatch(
-#'     check_crucial_names(x, "bad"),
-#'     missing_names = function(e) {
-#'       stop(
-#'         "A different error message",
-#'         call. = FALSE
-#'       )
-#'     }
-#'   )
-#' )
-#'
-#' testthat::expect_error(
-#'   check_crucial_names(x, "bad"),
-#'   class = "missing_names"
-#' )
+#' @noRd
 check_crucial_names <- function(x, expected_names) {
   stopifnot(rlang::is_named(x))
   stopifnot(is.character(expected_names))
@@ -62,7 +29,7 @@ abort_missing_names <- function(missing_names) {
     "missing_names",
     message = glue(
       "Must have missing names:
-    {usethis::ui_field(missing_names)}"
+      {paste0('`', missing_names, '`', collapse = ', ')}"
     )
   )
 }
