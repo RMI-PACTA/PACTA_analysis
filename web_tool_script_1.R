0d8b9636bae1258cf21178f7ea8121a4f21b66a0
Mauro Lepore <maurolepore@gmail.com>
2020-10-12 13:06:57 -0500

    Style
    r <- fs::dir_ls(regexp = "[.]R$", recurse = TRUE)
    rmd <- fs::dir_ls(regexp = "[.]Rmd$", recurse = TRUE)
    purrr::walk(c(r, rmd), styler::style_file)

--- web_tool_script_1.R
+++ web_tool_script_1.R
@@ -19,9 +19,9 @@ working_location <- paste0(working_location, "/")
 
 set_webtool_paths()
 
-options(r2dii_config = paste0(par_file_path,"/AnalysisParameters.yml"))
+options(r2dii_config = paste0(par_file_path, "/AnalysisParameters.yml"))
 
-set_global_parameters(paste0(par_file_path,"/AnalysisParameters.yml"))
+set_global_parameters(paste0(par_file_path, "/AnalysisParameters.yml"))
 
 # need to define an alternative location for data files
 analysis_inputs_path <- set_analysis_inputs_path(twodii_internal, data_location_ext, dataprep_timestamp)
@@ -36,7 +36,7 @@ analysis_inputs_path <- set_analysis_inputs_path(twodii_internal, data_location_
 # Set parameter to ensure data is reprocessed in "new_data" == TRUE in the parameter file
 file_location <- paste0(analysis_inputs_path, "cleaned_files")
 
-if(new_data == TRUE){
+if (new_data == TRUE) {
   currencies <- get_and_clean_currency_data()
 
   # fund_data <- get_and_clean_fund_data()
@@ -54,20 +54,17 @@ if(new_data == TRUE){
 
   company_emissions <- get_company_emission_data(inc_emission_factors)
 
-  save_cleaned_files(file_location,
-                     currencies,
-                     fund_data,
-                     fin_data,
-                     comp_fin_data,
-                     debt_fin_data,
-                     average_sector_intensity,
-                     company_emissions)
-
-
-
-
-}else{
-
+  save_cleaned_files(
+    file_location,
+    currencies,
+    fund_data,
+    fin_data,
+    comp_fin_data,
+    debt_fin_data,
+    average_sector_intensity,
+    company_emissions
+  )
+} else {
   currencies <- read_file(paste0(file_location, "/currencies.fst"))
 
   fund_data <- read_file(paste0(file_location, "/fund_data.fst"))
@@ -76,11 +73,11 @@ if(new_data == TRUE){
 
   comp_fin_data <- read_file(paste0(file_location, "/comp_fin_data.fst"))
 
-  debt_fin_data <- read_file(paste0(file_location,"/debt_fin_data.fst"))
+  debt_fin_data <- read_file(paste0(file_location, "/debt_fin_data.fst"))
 
   # revenue_data <- read_file(paste0(file_location, "revenue_data.fst"))
 
-  if (inc_emission_factors){
+  if (inc_emission_factors) {
     average_sector_intensity <- read_file(paste0(file_location, "/average_sector_intensity.fst"))
 
     company_emissions <- read_file(paste0(file_location, "/company_emissions.fst"))
@@ -91,11 +88,13 @@ if(new_data == TRUE){
 ####################
 portfolio_raw <- get_input_files(portfolio_name_ref_all)
 
-portfolio <- process_raw_portfolio(portfolio_raw,
-                                   fin_data,
-                                   fund_data,
-                                   currencies,
-                                   grouping_variables)
+portfolio <- process_raw_portfolio(
+  portfolio_raw,
+  fin_data,
+  fund_data,
+  currencies,
+  grouping_variables
+)
 
 portfolio <- add_revenue_split(has_revenue, portfolio, revenue_data)
 
@@ -103,11 +102,15 @@ portfolio <- create_ald_flag(portfolio, comp_fin_data, debt_fin_data)
 
 # portfolio <- add_bics_sector(portfolio)
 
-eq_portfolio <- create_portfolio_subset(portfolio,
-                                        "Equity")
+eq_portfolio <- create_portfolio_subset(
+  portfolio,
+  "Equity"
+)
 
-cb_portfolio <- create_portfolio_subset(portfolio,
-                                        "Bonds")
+cb_portfolio <- create_portfolio_subset(
+  portfolio,
+  "Bonds"
+)
 
 portfolio_total <- add_portfolio_flags(portfolio)
 
@@ -122,12 +125,14 @@ create_audit_chart(audit_file, proc_input_path)
 
 website_text(audit_file)
 
-emissions_totals <- calculate_portfolio_emissions(inc_emission_factors,
-                                                  audit_file,
-                                                  fin_data,
-                                                  comp_fin_data,
-                                                  average_sector_intensity,
-                                                  company_emissions)
+emissions_totals <- calculate_portfolio_emissions(
+  inc_emission_factors,
+  audit_file,
+  fin_data,
+  comp_fin_data,
+  average_sector_intensity,
+  company_emissions
+)
 
 ################
 #### SAVING ####
@@ -145,12 +150,14 @@ portfolio_name <- file_names$portfolio_name
 
 proc_input_path_ <- paste0(proc_input_path, "/", portfolio_name_ref_all)
 
-write_csv(file_names, paste0(proc_input_path_,"/file_names.csv"))
+write_csv(file_names, paste0(proc_input_path_, "/file_names.csv"))
 
 
-export_audit_information_jsons(audit_file_ = audit_file %>% filter(portfolio_name == portfolio_name),
-                               portfolio_total_ = portfolio_total %>% filter(portfolio_name == portfolio_name),
-                               folder_path = proc_input_path_)
+export_audit_information_jsons(
+  audit_file_ = audit_file %>% filter(portfolio_name == portfolio_name),
+  portfolio_total_ = portfolio_total %>% filter(portfolio_name == portfolio_name),
+  folder_path = proc_input_path_
+)
 
 save_if_exists(audit_file, portfolio_name, paste0(proc_input_path_, "/audit_file.csv"), csv_or_rds = "csv")
 
@@ -160,4 +167,3 @@ save_if_exists(cb_portfolio, portfolio_name, paste0(proc_input_path_, "/bonds_po
 save_if_exists(portfolio_overview, portfolio_name, paste0(proc_input_path_, "/overview_portfolio.rda"))
 save_if_exists(audit_file, portfolio_name, paste0(proc_input_path_, "/audit_file.rda"))
 save_if_exists(emissions_totals, portfolio_name, paste0(proc_input_path_, "/emissions.rda"))
-
