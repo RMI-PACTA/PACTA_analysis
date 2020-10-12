693df2dfc44f4ce02f1c329ecce471468fff1edc
Mauro Lepore <maurolepore@gmail.com>
2020-10-12 06:52:52 -0500

    Refactor update_dockerfile_packages()

--- R/utils.R
+++ R/utils.R
@@ -21,35 +21,38 @@ use_r_packages <- function(path = packages_path()) {
   invisible(path)
 }
 
-packages_path <- function() {
-  "deduplicate/load-and-attach-r-packages.R"
+this_repo <- function() {
+  ifelse(in_transitionmonitor(), "bound", "PACTA_analysis")
 }
 
-update_dockerfile_packages <- function(path = "docker/2diirunner-with-packages/Dockerfile") {
-  dkr <- readLines(path, encoding = "UTF-8")
-
+update_dockerfile_packages <- function(path = dockerfile_path()) {
   raw <- readLines(packages_path(), encoding = "UTF-8")
   pkg <- sub("library\\((.*)\\)", "\\1", raw)
   pkg <- glue('    && Rscript -e "install.packages(\'{pkg}\')" \\')
 
-  out <- c(
-    dkr[1:marker_start(dkr)],
+  dkr <- readLines(path, encoding = "UTF-8")
+  dkr_updated <- c(
+    dkr[1:end_of_packages_on_dockerfile(dkr)],
     pkg,
-    dkr[marker_end(dkr):length(dkr)]
+    dkr[start_of_packages_on_dockerfile(dkr):length(dkr)]
   )
-  writeLines(out, path)
+  writeLines(dkr_updated, path)
 
   invisible(path)
 }
 
-marker_start <- function(lines) {
-  grep("# update-dockerfile-packages-start", lines)
+packages_path <- function() {
+  "deduplicate/load-and-attach-r-packages.R"
 }
 
-marker_end <- function(lines) {
+dockerfile_path <- function() {
+  "docker/2diirunner-with-packages/Dockerfile"
+}
+
+start_of_packages_on_dockerfile <- function(lines) {
   grep("# update-dockerfile-packages-end", lines)
 }
 
-this_repo <- function() {
-  ifelse(in_transitionmonitor(), "bound", "PACTA_analysis")
+end_of_packages_on_dockerfile <- function(lines) {
+  grep("# update-dockerfile-packages-start", lines)
 }
