2e708bcb70fc68f1f9e27d6661a4b036620b5488
Clare2D <clare@2degrees-investing.org>
2020-10-12 14:27:27 +0200

    moved data call into check

--- web_tool_script_2.R
+++ web_tool_script_2.R
@@ -1,14 +1,26 @@
-devtools::load_all()
-use_r_packages()
 
 #########################################################################
 # START RUN ANALYIS
 #########################################################################
 
+library(tidyr)
+library(dplyr)
+library(scales)
+library(reshape2)
+library(tidyverse)
+library(readxl)
+library(tidyselect)
+library(r2dii.utils)
+library(fs)
+library(jsonlite)
+library(fst)
+library(here)
+
 source("0_portfolio_test.R")
 source("0_global_functions.R")
 source("0_web_functions.R")
 
+devtools::load_all()
 setup_project()
 
 working_location <- paste0(working_location, "/")
@@ -16,15 +28,15 @@ working_location <- paste0(working_location, "/")
 set_webtool_paths()
 
 # just done once
-options(r2dii_config = paste0(par_file_path, "/AnalysisParameters.yml"))
+options(r2dii_config = paste0(par_file_path,"/AnalysisParameters.yml"))
 
-set_global_parameters(paste0(par_file_path, "/AnalysisParameters.yml"))
+set_global_parameters(paste0(par_file_path,"/AnalysisParameters.yml"))
 
 # need to define an alternative location for data files
 analysis_inputs_path <- set_analysis_inputs_path(twodii_internal, data_location_ext, dataprep_timestamp)
 
 # delete all results files within the current portfolio folder
-unlink(paste0(results_path, "/", portfolio_name_ref_all, "/*"), force = TRUE, recursive = TRUE)
+unlink(paste0(results_path,"/",portfolio_name_ref_all,"/*"), force = TRUE, recursive = TRUE)
 
 # run again so output folders are available after deleting past results
 file_names <- read_csv(paste0(proc_input_path, "/", portfolio_name_ref_all, "/file_names.csv"))
@@ -36,23 +48,24 @@ port_col_types <- set_col_types(grouping_variables, "ddddccccddclc")
 ##### EQUITY #####
 ##################
 
-ald_scen_eq <- get_ald_scen("Equity")
-ald_raw_eq <- get_ald_raw("Equity")
 
-equity_input_file <- paste0(proc_input_path, "/", portfolio_name_ref_all, "/equity_portfolio.rda")
+
+equity_input_file <- paste0(proc_input_path,"/", portfolio_name_ref_all, "/equity_portfolio.rda")
 portfolio_name <- file_names$portfolio_name
 
-if (file.exists(equity_input_file)) {
-  port_raw_all_eq <- read_rds(equity_input_file) %>%
+if(file.exists(equity_input_file)){
+
+  ald_scen_eq <- get_ald_scen("Equity")
+  ald_raw_eq <- get_ald_raw("Equity")
+
+    port_raw_all_eq <- read_rds(equity_input_file) %>%
     mutate(id = as.character(id))
 
-  if (length(colnames(port_raw_all_eq)) != nchar(port_col_types)) {
-    stop("Check port_col_types: difference in length")
-  }
+  if(length(colnames(port_raw_all_eq)) != nchar(port_col_types)){stop("Check port_col_types: difference in length")}
 
   list_investors_eq <- unique(port_raw_all_eq$investor_name)
 
-  for (e in 1:length(list_investors_eq)) {
+  for (e in 1:length(list_investors_eq) ){
     map_eq <- NA
     company_all_eq <- NA
     port_all_eq <- NA
@@ -85,10 +98,12 @@ if (file.exists(equity_input_file)) {
 
     port_all_eq <- bind_rows(port_pw_eq, port_own_eq)
 
-    if (has_map) {
+    if(has_map){
+
       map_eq <- merge_in_geography(company_all_eq, ald_raw_eq)
 
       map_eq <- aggregate_map_data(map_eq)
+
     }
 
     # Technology Share Calculation
@@ -101,22 +116,12 @@ if (file.exists(equity_input_file)) {
 
     company_all_eq <- calculate_scenario_alignment(company_all_eq)
 
-    pf_file_results_path <- paste0(results_path, "/", portfolio_name_ref_all, "/")
-    if (!dir.exists(pf_file_results_path)) {
-      dir.create(pf_file_results_path)
-    }
+    pf_file_results_path <- paste0(results_path,"/", portfolio_name_ref_all, "/")
+    if(!dir.exists(pf_file_results_path)){dir.create(pf_file_results_path)}
 
-    if (data_check(company_all_eq)) {
-      write_rds(company_all_eq, paste0(pf_file_results_path, "Equity_results_company.rda"))
-    }
-    if (data_check(port_all_eq)) {
-      write_rds(port_all_eq, paste0(pf_file_results_path, "Equity_results_portfolio.rda"))
-    }
-    if (has_map) {
-      if (data_check(map_eq)) {
-        write_rds(map_eq, paste0(pf_file_results_path, "Equity_results_map.rda"))
-      }
-    }
+    if(data_check(company_all_eq)){write_rds(company_all_eq, paste0(pf_file_results_path, "Equity_results_company.rda"))}
+    if(data_check(port_all_eq)){write_rds(port_all_eq, paste0(pf_file_results_path, "Equity_results_portfolio.rda"))}
+    if(has_map){if(data_check(map_eq)){write_rds(map_eq, paste0(pf_file_results_path, "Equity_results_map.rda"))}}
   }
 }
 
@@ -125,23 +130,22 @@ if (file.exists(equity_input_file)) {
 ##### BONDS #####
 #################
 
-ald_scen_cb <- get_ald_scen("Bonds")
-ald_raw_cb <- get_ald_raw("Bonds")
-
-bonds_inputs_file <- paste0(proc_input_path, "/", portfolio_name_ref_all, "/bonds_portfolio.rda")
+bonds_inputs_file <- paste0(proc_input_path,"/", portfolio_name_ref_all,"/bonds_portfolio.rda")
 portfolio_name <- file_names$portfolio_name
 
-if (file.exists(bonds_inputs_file)) {
+if (file.exists(bonds_inputs_file)){
+
+  ald_scen_cb <- get_ald_scen("Bonds")
+  ald_raw_cb <- get_ald_raw("Bonds")
+
   port_raw_all_cb <- read_rds(bonds_inputs_file) %>%
     mutate(id = as.character(id))
 
-  if (length(colnames(port_raw_all_cb)) != nchar(port_col_types)) {
-    stop("Check port_col_types: difference in length")
-  }
+  if(length(colnames(port_raw_all_cb)) != nchar(port_col_types)){stop("Check port_col_types: difference in length")}
 
   list_investors_cb <- unique(port_raw_all_cb$investor_name)
 
-  for (b in 1:length(list_investors_cb)) {
+  for (b in 1:length(list_investors_cb) ){
     map_cb <- NA
     company_all_cb <- NA
     port_all_cb <- NA
@@ -168,8 +172,9 @@ if (file.exists(bonds_inputs_file)) {
 
     port_all_cb <- port_pw_cb
 
-    if (has_map) {
-      if (data_check(company_all_cb)) {
+    if(has_map){
+
+      if(data_check(company_all_cb)){
         map_cb <- merge_in_geography(company_all_cb, ald_raw_cb)
 
         map_cb <- aggregate_map_data(map_cb)
@@ -177,38 +182,21 @@ if (file.exists(bonds_inputs_file)) {
     }
 
     # Technology Share Calculation
-    if (nrow(port_all_cb) > 0) {
-      port_all_cb <- calculate_technology_share(port_all_cb)
-    }
+    if(nrow(port_all_cb)>0){port_all_cb <- calculate_technology_share(port_all_cb)}
 
-    if (nrow(company_all_cb) > 0) {
-      company_all_cb <- calculate_technology_share(company_all_cb)
-    }
+    if(nrow(company_all_cb)>0){company_all_cb <- calculate_technology_share(company_all_cb)}
 
     # Scenario alignment calculations
     port_all_cb <- calculate_scenario_alignment(port_all_cb)
 
     company_all_cb <- calculate_scenario_alignment(company_all_cb)
 
-    pf_file_results_path <- paste0(results_path, "/", portfolio_name_ref_all, "/")
-    if (!dir.exists(pf_file_results_path)) {
-      dir.create(pf_file_results_path)
-    }
+    pf_file_results_path <- paste0(results_path,"/", portfolio_name_ref_all, "/")
+    if(!dir.exists(pf_file_results_path)){dir.create(pf_file_results_path)}
 
-    if (data_check(company_all_cb)) {
-      write_rds(company_all_cb, paste0(pf_file_results_path, "Bonds_results_company.rda"))
-    }
-    if (data_check(port_all_cb)) {
-      write_rds(port_all_cb, paste0(pf_file_results_path, "Bonds_results_portfolio.rda"))
-    }
-    if (has_map) {
-      if (data_check(map_cb)) {
-        write_rds(map_cb, paste0(pf_file_results_path, "Bonds_results_map.rda"))
-      }
-    }
+    if(data_check(company_all_cb)){ write_rds(company_all_cb, paste0(pf_file_results_path, "Bonds_results_company.rda"))}
+    if(data_check(port_all_cb)){write_rds(port_all_cb, paste0(pf_file_results_path, "Bonds_results_portfolio.rda"))}
+    if(has_map){if(data_check(map_cb)){write_rds(map_cb, paste0(pf_file_results_path, "Bonds_results_map.rda"))}}
   }
 }
 
-invisible(set_portfolio_parameters(file_path = fs::path(par_file_path, paste0(portfolio_name_ref_all, "_PortfolioParameters.yml"))))
-
-source(paste0(stress_test_path, "web_tool_stress_test.R"))
