18b842286411c8edb20ce8b7b8e15d03a291b5dc
Mauro Lepore <maurolepore@gmail.com>
2020-10-13 15:55:43 -0500

    New update_dockerfile_packages()

--- R/utils.R
+++ R/utils.R
@@ -13,7 +13,7 @@ setup_project <- function() {
   source(path)
 }
 
-use_r_packages <- function(path = "deduplicate/load-and-attach-r-packages.R") {
+use_r_packages <- function(path = packages_path()) {
   source(path)
   resolve_conflicts()
 
@@ -28,4 +28,61 @@ resolve_conflicts <- function() {
   conflicted::conflict_prefer("rename", "dplyr")
   conflicted::conflict_prefer("summarise", "dplyr")
   conflicted::conflict_prefer("arrange", "dplyr")
+}
+
+update_dockerfile_packages <- function(path = dockerfile_path()) {
+  old_dockerfile <- read_dockerfile(path)
+  new_dockerfile <- c(
+    dockerfile_head(old_dockerfile),
+    dockerfile_packages(packages_path()),
+    dockerfile_tail(old_dockerfile)
+  )
+  writeLines(new_dockerfile, path)
+
+  invisible(path)
+}
+
+read_dockerfile <- function(path = dockerfile_path()) {
+  readLines(path, encoding = "UTF-8")
+}
+
+dockerfile_head <- function(dockerfile = read_dockerfile()) {
+  dockerfile[1:end_of_packages_on_dockerfile(dockerfile)]
+}
+
+dockerfile_tail <- function(dockerfile = read_dockerfile()) {
+  dockerfile[start_of_packages_on_dockerfile(dockerfile):length(dockerfile)]
+}
+
+dockerfile_packages <- function(path = packages_path()) {
+  raw <- readLines(path, encoding = "UTF-8")
+  pkg <- sub("library\\((.*)\\)", "\\1", raw)
+
+  c(
+    '    && Rscript -e "install.packages( \\',
+    paste0('             ', format_as_vector(pkg), ' \\'),
+    '           )" \\'
+  )
+}
+
+format_as_vector <- function(string) {
+  x <- glue("'{string}',")
+  x[length(x)] <- sub(",$", "", x[length(x)])
+  c('c(', glue("  {x}"), ')' )
+}
+
+packages_path <- function() {
+  file.path("deduplicate", "load-and-attach-r-packages.R")
+}
+
+dockerfile_path <- function() {
+  file.path("docker", "2diirunner-with-packages", "Dockerfile")
+}
+
+start_of_packages_on_dockerfile <- function(lines) {
+  grep("# update-dockerfile-packages-end", lines)
+}
+
+end_of_packages_on_dockerfile <- function(lines) {
+  grep("# update-dockerfile-packages-start", lines)
 }
