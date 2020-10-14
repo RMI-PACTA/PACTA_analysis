7c3681e1e78ec23b39b24610d971fa4272d2e0d5
Mauro Lepore <maurolepore@gmail.com>
2020-10-14 14:37:48 -0500

    Use base R implementation

--- R/test_r_scripts.R
+++ R/test_r_scripts.R
@@ -1,6 +1,14 @@
 test_r_scripts <- function(x = 1:3) {
-  command <- glue("Rscript --vanilla web_tool_script_{x}.R TestPortfolio_Input")
-  purrr::walk(command, system)
+  command <- r_script_command(x)
+
+  for (i in seq_along(command)) {
+    message("Testing: ", command[[i]])
+    system(command[[i]])
+  }
 
   invisible(x)
+}
+
+r_script_command <- function(x) {
+  sprintf("Rscript --vanilla web_tool_script_%s.R TestPortfolio_Input", x)
 }
