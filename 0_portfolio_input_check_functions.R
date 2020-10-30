4add66298c02dc29f7712e5cc45dc108f1bfba34
jacobvjk <jacob.kastl@gmail.com>
2020-10-30 11:30:17 +0100

    add named args to all function calls of write_log()

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -63,35 +63,40 @@ clean_portfolio_col_types <- function(portfolio, grouping_variables) {
       "Wrong variable class for investor_name. Should be character, but is ",
       class(portfolio$investor_name),
       ". This can introduce errors in further calculations!"
-    ))
+    ),
+    file_path = log_path)
   }
   if (is.character(portfolio$portfolio_name) == FALSE) {
     write_log(msg = paste0(
       "Wrong variable class for portfolio_name Should be character, but is ",
       class(portfolio$portfolio_name),
       ". This can introduce errors in further calculations!"
-    ))
+    ),
+    file_path = log_path)
   }
   if (is.numeric(portfolio$market_value) == FALSE) {
     write_log(msg = paste0(
       "Wrong variable class for market_value Should be numeric, but is ",
       class(portfolio$market_value),
       ". This can introduce errors in further calculations!"
-    ))
+    ),
+    file_path = log_path)
   }
   if (is.character(portfolio$currency) == FALSE) {
     write_log(msg = paste0(
       "Wrong variable class for currency Should be character, but is ",
       class(portfolio$currency),
       ". This can introduce errors in further calculations!"
-    ))
+    ),
+    file_path = log_path)
   }
   if (is.character(portfolio$isin) == FALSE) {
     write_log(msg = paste0(
       "Wrong variable class for isin Should be character, but is ",
       class(portfolio$isin),
       ". This can introduce errors in further calculations!"
-    ))
+    ),
+    file_path = log_path)
   }
   ### what about number_of_shares???
 
@@ -112,7 +117,8 @@ clear_portfolio_input_blanks <- function(portfolio) {
               because of missing values in at least one of the variables", str_c(grouping_variables, collapse = ", "),
       "\n To ensure complete analysis, please upload a file without
                           missing values in these columns."
-    ))
+    ),
+    file_path = log_path)
 
     portfolio <- portfolio %>% filter_at(
       grouping_variables, all_vars(!is.na(.))
@@ -191,7 +197,10 @@ check_missing_cols <- function(portfolio, grouping_variables) {
   missing_columns <- setdiff(required_input_cols, colnames(portfolio))
 
   if (length(missing_columns) > 0) {
-    write_log(msg = paste0("The input file is missing the following data columns: ", missing_columns))
+    write_log(
+      msg = paste0("The input file is missing the following data columns: ", missing_columns),
+      file_path = log_path
+      )
     stop(paste0("The input file is missing the following data columns: ", missing_columns))
   }
 
