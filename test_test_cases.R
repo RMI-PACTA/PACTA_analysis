a2379dd09961cf772e8573ed611b065ddd0b0a35
CJ Yetman <cj@2degrees-investing.org>
2020-11-03 11:48:36 +0100

    fix the path which got messed up in the PR somehow (#304)

--- test_test_cases.R
+++ test_test_cases.R
@@ -2,7 +2,7 @@ library(fs)
 library(readr)
 library(yaml)
 
-test_cases_dir <- "~/Dropbox (2° Investing)/2° Investing Team/People/Jacob/p2020_test_casesss"
+test_cases_dir <- "~/Dropbox (2° Investing)/2° Investing Team/People/Jacob/p2020_test_cases"
 stopifnot(fs::dir_exists(test_cases_dir))
 
 test_cases_csvs <- fs::dir_ls(test_cases_dir, regexp = "[.]csv")
