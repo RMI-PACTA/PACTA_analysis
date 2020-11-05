163ab9683f0305e10a3b59379d45299e2b6cc5e8
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-11-05 11:32:47 +0100

    style

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -1311,14 +1311,14 @@ calculate_average_portfolio_emissions <- function(portfolio_total,
     left_join(
       select(comp_fin_data, company_id, bics_sector),
       by = "company_id"
-      ) %>%
+    ) %>%
     group_by(
       investor_name,
       portfolio_name,
       asset_type,
       financial_sector,
       bics_sector
-      ) %>%
+    ) %>%
     summarise(value_usd = sum(value_usd, na.rm = T),  .groups = "drop") %>%
     left_join(
       select(
@@ -1327,9 +1327,9 @@ calculate_average_portfolio_emissions <- function(portfolio_total,
         asset_type,
         median_intensity,
         mean_intensity
-        ),
+      ),
       by = c("bics_sector", "asset_type")
-      )
+    )
 
   # Create averages where bics sectors were missing
   average_other_sectors <- min_portfolio %>%
@@ -1337,20 +1337,20 @@ calculate_average_portfolio_emissions <- function(portfolio_total,
     summarise(
       mean_intensity = mean(mean_intensity, na.rm = T),
       .groups = "drop"
-      )
+    )
 
   min_portfolio <- min_portfolio %>%
     left_join(
       average_other_sectors,
       by = c("asset_type", "financial_sector")
-      ) %>%
+    ) %>%
     mutate(
       mean_intensity = ifelse(
         is.na(mean_intensity.x),
         mean_intensity.y,
         mean_intensity.x
-        )
-      ) %>%
+      )
+    ) %>%
     select(-mean_intensity.x, -mean_intensity.y)  %>%
     mutate(weighted_sector_emissions = value_usd * mean_intensity)
 
@@ -1360,18 +1360,18 @@ calculate_average_portfolio_emissions <- function(portfolio_total,
         financial_sector != "Other",
         financial_sector,
         bics_sector
-        ),
+      ),
       sector = ifelse(
         is.na(sector),
         "Other",
         sector
-        )
-      ) %>%
+      )
+    ) %>%
     group_by(investor_name, portfolio_name, asset_type,sector) %>%
     summarise(
       weighted_sector_emissions = sum(weighted_sector_emissions, na.rm = T),
       .groups = "drop"
-      )
+    )
 
 
 
