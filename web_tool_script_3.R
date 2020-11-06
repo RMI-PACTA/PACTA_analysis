168cd4122b0b046d74645321ca9b1ee47ccd4f08
CJ Yetman <cj@2degrees-investing.org>
2020-11-06 12:48:47 +0100

    leave the default TestPortfolio_Input

--- web_tool_script_3.R
+++ web_tool_script_3.R
@@ -6,7 +6,7 @@ use_r_packages()
 source("0_global_functions.R")
 source("0_web_functions.R")
 
-if (!exists("portfolio_name_ref_all")) { portfolio_name_ref_all <- "PensionFunds_Input" }
+if (!exists("portfolio_name_ref_all")) { portfolio_name_ref_all <- "TestPortfolio_Input" }
 if (!exists("portfolio_root_dir")) { portfolio_root_dir <- "working_dir" }
 
 setup_project()
