33d56b139a4048e891dbeba104b25510b9d69995
CJ Yetman <cj@2degrees-investing.org>
2020-11-19 12:16:18 +0100

    rework the concept

--- clean_data.R
+++ clean_data.R
@@ -1,32 +1,30 @@
-read_rds_or_rda <-
-  function(file_path) {
-    tryCatch(
-      readRDS(file_path),
-      error = function(e) read_rdata_as_rda(file_path)
-    )
-  }
-
-
-read_rdata_as_rda <-
-  function(file_path) {
-    tmp_env <- new.env()
-    obj_list <- load(file_path, envir = tmp_env)
-    stopifnot(length(obj_list) == 1)
-    tmp_env[[obj_list]]
-  }
+get_currency_data <- function() {
+  readRDS("data/currencies.rds")
+}
 
 
 get_currency_data_for_timestamp <-
-  function(currency_data_path = "data/currencies.rda",
-           financial_timestamp = c(parent.frame()$financial_timestamp, "2019Q4")) {
+  function(financial_timestamp,
+           currency_data = NULL,
+           exchange_rate_colname = paste0("ExchangeRate_", financial_timestamp),
+           currency_code_colname = "Currency_abbr") {
+    if (is.null(currency_data)) {
+      currency_data <- get_currency_data()
+    }
 
-    suppressPackageStartupMessages(require(dplyr))
-    stopifnot(file.exists(currency_data_path))
+    stopifnot(inherits(currency_data, "data.frame"))
+    stopifnot(currency_code_colname %in% names(currency_data))
+    stopifnot(exchange_rate_colname %in% names(currency_data))
+    stopifnot(inherits(currency_data[[currency_code_colname]], "character"))
+    stopifnot(inherits(currency_data[[exchange_rate_colname]], "numeric"))
 
-    read_rds_or_rda(currency_data_path) %>%
-      select(currency = Currency_abbr,
-             exchange_rate = paste0("ExchangeRate_", financial_timestamp)) %>%
-      filter(!is.na(currency) & currency != "") %>%
-      distinct() %>%
-      mutate(exchange_rate = as.numeric(exchange_rate))
-}
+    currency_data <-
+      currency_data[, c(currency_code_colname, exchange_rate_colname)]
+    names(currency_data) <- c("currency", "exchange_rate")
+    currency_data <-
+      currency_data[!is.na(currency_data$currency) &
+                      currency_data$currency != "",]
+    currency_data <- currency_data[!duplicated(currency_data),]
+
+    currency_data
+  }
--- tests/testthat/test-clean_data.R
+++ tests/testthat/test-clean_data.R
@@ -4,7 +4,7 @@ source("clean_data.R")
 
 # get_currency_data_for_timestamp() --------------------------------------------
 
-result <- get_currency_data_for_timestamp()
+result <- get_currency_data_for_timestamp("2019Q4")
 
 test_that("get_currency_data_for_timestamp() function exists", {
   expect_false(
@@ -12,38 +12,38 @@ test_that("get_currency_data_for_timestamp() function exists", {
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble", {
+test_that("get_currency_data_for_timestamp() returns a data.frame", {
   expect_true(
-    inherits(result, "tbl_df")
+    inherits(result, "data.frame")
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble with 2 columns", {
+test_that("get_currency_data_for_timestamp() returns a data.frame with 2 columns", {
   expect_true(
     ncol(result) == 2
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble with the proper column names", {
+test_that("get_currency_data_for_timestamp() returns a data.frame with the proper column names", {
   expect_equal(
     names(result),
     c("currency", "exchange_rate")
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble with a character class currency column", {
+test_that("get_currency_data_for_timestamp() returns a data.frame with a character class currency column", {
   expect_true(
     inherits(result$currency, "character")
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble with a currency column that does not contain any duplicates", {
+test_that("get_currency_data_for_timestamp() returns a data.frame with a currency column that does not contain any duplicates", {
   expect_false(
     any(duplicated(result$currency))
   )
 })
 
-test_that("get_currency_data_for_timestamp() returns a tibble with a numeric class exchange_rate column", {
+test_that("get_currency_data_for_timestamp() returns a data.frame with a numeric class exchange_rate column", {
   expect_true(
     inherits(result$exchange_rate, "numeric")
   )
