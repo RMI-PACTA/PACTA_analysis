fd5f9be270cf5d67f9c4c61f78cdebccd7c40356
CJ Yetman <cj@2degrees-investing.org>
2020-11-22 00:30:02 +0100

    improved tests for convert_industry_classification

--- tests/clean_data_tests/test-convert_industry_classification.R
+++ tests/clean_data_tests/test-convert_industry_classification.R
@@ -1,9 +1,27 @@
 setwd(here::here())
 function_name <- "convert_industry_classification"
-result <- convert_industry_classification(c("XXX"), from = "BICS", to = "sector")
+
 
 test_that(paste0(function_name, "() function exists"), {
   expect_true(
     exists(function_name)
+  )
+})
+
+test_that(paste0(function_name, "() returns NA for an unknown string"), {
+  expect_true(
+    is.na(convert_industry_classification("XXX", from = "BICS", to = "sector"))
+  )
+})
+
+test_that(paste0(function_name, "() returns an appropriately matched string for an exisitng BICS group"), {
+  expect_true(
+    convert_industry_classification("Advertising Agencies", from = "BICS", to = "sector") == "Other"
+  )
+})
+
+test_that(paste0(function_name, "() returns an appropriately matched string for an exisitng ICB group"), {
+  expect_true(
+    convert_industry_classification("Aerospace", from = "ICB", to = "sector") == "Other"
   )
 })
