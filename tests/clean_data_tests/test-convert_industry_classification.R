40ec9f3ec1db4b5a87ac58db405fffade070a86e
CJ Yetman <cj@2degrees-investing.org>
2020-11-22 00:51:38 +0100

    figure out what's wrong with the test

--- tests/clean_data_tests/test-convert_industry_classification.R
+++ tests/clean_data_tests/test-convert_industry_classification.R
@@ -1,5 +1,6 @@
 setwd(here::here())
 function_name <- "convert_industry_classification"
+cat("\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\ntest\n")
 
 
 test_that(paste0(function_name, "() function exists"), {
