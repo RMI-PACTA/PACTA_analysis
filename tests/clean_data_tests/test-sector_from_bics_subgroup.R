58d1f40f4255731704f86ee51c42123871f0f661
CJ Yetman <cj@2degrees-investing.org>
2020-11-21 23:58:33 +0100

    improved tests for matching functions

--- tests/clean_data_tests/test-sector_from_bics_subgroup.R
+++ tests/clean_data_tests/test-sector_from_bics_subgroup.R
@@ -1,9 +1,21 @@
 setwd(here::here())
 function_name <- "sector_from_bics_subgroup"
-result <- sector_from_bics_subgroup(c("XXX"))
+
 
 test_that(paste0(function_name, "() function exists"), {
   expect_true(
     exists(function_name)
+  )
+})
+
+test_that(paste0(function_name, "() returns NA for an unknown string"), {
+  expect_true(
+    is.na(sector_from_bics_subgroup("XXX"))
+  )
+})
+
+test_that(paste0(function_name, "() returns an appropriately matched string for an exisitng BICS group"), {
+  expect_true(
+    sector_from_bics_subgroup("Advertising Agencies") == "Other"
   )
 })
--- tests/clean_data_tests/test-sector_from_icb_subgroup.R
+++ tests/clean_data_tests/test-sector_from_icb_subgroup.R
@@ -1,9 +1,21 @@
 setwd(here::here())
 function_name <- "sector_from_icb_subgroup"
-result <- sector_from_icb_subgroup(c("XXX"))
+
 
 test_that(paste0(function_name, "() function exists"), {
   expect_true(
     exists(function_name)
+  )
+})
+
+test_that(paste0(function_name, "() returns NA for an unknown string"), {
+  expect_true(
+    is.na(sector_from_icb_subgroup("XXX"))
+  )
+})
+
+test_that(paste0(function_name, "() returns an appropriately matched string for an exisitng ICB group"), {
+  expect_true(
+    sector_from_icb_subgroup("Aerospace") == "Other"
   )
 })
