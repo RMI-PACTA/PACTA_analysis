1b92b87a5974df0289be582d8819910f1811438a
Mauro Lepore <maurolepore@gmail.com>
2020-11-24 15:12:36 -0600

    New function to skip check() but run test() (#347)
    This allows using here::here() in tests.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -15,3 +15,14 @@ test_that("use_r_packages() puts listed packages on search() path", {
     expect_true(is_attached(package))
   }
 })
+
+test_that("helper skip_check_but_run_test() works as expected", {
+  # This test should not be skipped during devtools::test()
+  # This test should be skipped during devtools::check()
+  # If you remove `skip_check_but_run_test()`, devtools::check() should error
+  skip_check_but_run_test()
+
+  # This file should be available only when the source code is accessible, i.e.
+  # not during R CMD check, when the code runs from the system installation
+  expect_true(fs::file_exists(here::here(".pacta")))
+})
