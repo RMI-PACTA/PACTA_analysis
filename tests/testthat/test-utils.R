c54fde8eb7bf30d4e5d18c6e6a14fa394195545f
Mauro Lepore <maurolepore@gmail.com>
2020-11-24 09:51:06 -0600

    Test abort_if_file_doesnt_exist()

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -15,3 +15,12 @@ test_that("use_r_packages() puts listed packages on search() path", {
     expect_true(is_attached(package))
   }
 })
+
+test_that("abort_if_file_doesnt_exist does what its name promises", {
+  file <- tempfile()
+  expect_error(abort_if_file_doesnt_exist(file))
+
+  fs::file_create(file)
+  withr::defer(fs::file_delete(file))
+  expect_error(abort_if_file_doesnt_exist(file), NA)
+})
