cd22878ed8aa0bf2ca2f10673b440962764c98a6
Mauro Lepore <maurolepore@gmail.com>
2020-11-24 06:29:09 -0600

    Collect feedback

--- tests/testthat/test-test_r_scripts.R
+++ tests/testthat/test-test_r_scripts.R
@@ -6,13 +6,8 @@ test_that("stop_on_error stops on error", {
 })
 
 test_that("example data is available under sample_data/", {
-  # FIXME: @jacobvjk, can you explain the role of this file?
-  # At https://bit.ly/35Xypsw I read:
-  # > copy TestPortfolio_Input.csv from sample_files/20_input_files/ to
-  # > working_dir/20_Raw_Inputs/
-  # Do we copy that file manually or programmatically? If so, where is the code?
-  # My experiments suggest web_tool_script_1.R runs to the end without it, and
-  # I need to know it that's okay of if I should throw an error ASAP.
+  # TODO: Make this available in inst/extdate with an example helper?
+  # -- https://git.io/JkX5O
   example_data <- here::here(
     "sample_files", "20_input_files", "TestPortfolio_Input.csv"
   )
@@ -20,6 +15,10 @@ test_that("example data is available under sample_data/", {
 })
 
 test_that("example data is available under working_dir/", {
+  # FIXME: Replace this with a a function that gets the path from
+  # working_dir/10_Parameter_File/TestPortfolio_Input_PortfolioParameters.yml
+  # portfolio_name_in: TestPortfolio_Input
+  # -- https://git.io/JkX5O
   example_data <- here::here(
     "working_dir", "20_Raw_Inputs", "TestPortfolio_Input.csv"
   )
