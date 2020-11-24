8cfcf144b0b950dc290068b5364dd31bfdbf1eea
CJ Yetman <cj@2degrees-investing.org>
2020-11-24 13:56:13 +0100

    use function name directly

--- tests/testthat/test-get_and_clean_company_fin_data.R
+++ tests/testthat/test-get_and_clean_company_fin_data.R
@@ -1,15 +1,13 @@
 setwd(here::here())
-function_name <- "get_and_clean_company_fin_data"
-result <- get_and_clean_company_fin_data("inst/extdata")
 
-test_that(paste0(function_name, "() function exists"), {
+test_that("get_and_clean_company_fin_data() function exists", {
   expect_true(
-    exists(function_name)
+    class(get_and_clean_company_fin_data) == "function"
   )
 })
 
-test_that(paste0(function_name, "() returns a data.frame"), {
+test_that("get_and_clean_company_fin_data() returns a data.frame", {
   expect_true(
-    inherits(result, "data.frame")
+    inherits(get_and_clean_company_fin_data("inst/extdata"), "data.frame")
   )
 })
