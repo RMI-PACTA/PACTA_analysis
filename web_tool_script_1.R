3ca01b3b329010a4f7181794a95208d2172aa336
Clare2D <clare@2degrees-investing.org>
2020-11-25 17:26:08 +0100

    more cleaning

--- web_tool_script_1.R
+++ web_tool_script_1.R
@@ -19,10 +19,6 @@ working_location <- file.path(working_location)
 
 set_webtool_paths(portfolio_root_dir)
 
-# options(r2dii_config = file.path(working_location, "parameter_files", "AnalysisParameters.yml"))
-#
-# set_global_parameters(file.path(working_location, "parameter_files", "AnalysisParameters.yml"))
-
 set_portfolio_parameters(file_path = fs::path(par_file_path, paste0(portfolio_name_ref_all, "_PortfolioParameters.yml")))
 
 set_project_parameters(file.path(working_location, "parameter_files",paste0("ProjectParameters_", project_code, ".yml")))
