2f4405e97be692d72d2de2d7c85bc7965a6aba34
CJ Yetman <cj@2degrees-investing.org>
2020-11-26 18:15:36 +0100

    add more common file names

--- R/values.R
+++ R/values.R
@@ -23,7 +23,7 @@ default_filenames_sans_ext <-
     company_emissions = "company_emissions",
     consolidated_financial = "consolidated_financial",
     debt_financial = "debt_financial",
-    exchange_rates = "currencies",
+    exchange_rates = "exchange_rates",
     fin_sector_overrides = "fin_sector_overrides",
     funds = "funds",
     non_distinct_isins = "non_distinct_isins",
@@ -34,18 +34,18 @@ default_filenames_sans_ext <-
 
 common_filenames_sans_ext <-
   list(
-    average_sector_intensity = "average_sector_intensity",
-    bics_bridge = "bics_bridge",
-    company_emissions = "company_emissions",
+    average_sector_intensity = c("average_sector_intensity", "average_sector_intensity_data"),
+    bics_bridge = c("bics_bridge", "bics_bridge_data"),
+    company_emissions = c("company_emissions", "company_emissions_data"),
     consolidated_financial = c("consolidated_financial", "consolidated_financial_data"),
-    debt_financial = "debt_financial_data",
-    exchange_rates = c("currencies", "currency_data", "exchange_rates"),
-    fin_sector_overrides = "fin_sector_overrides",
-    funds = "funds",
-    non_distinct_isins = "non_distinct_isins",
-    revenue = "revenue_data",
-    sector_bridge = "sector_bridge",
-    security_financial = "security_financial_data"
+    debt_financial = c("debt_financial", "debt_financial_data"),
+    exchange_rates = c("exchange_rates", "exchange_rates_data", "currencies", "currencies_data", "currency_data"),
+    fin_sector_overrides = c("fin_sector_overrides", "fin_sector_overrides_data"),
+    funds = c("funds", "funds_data", "fund_data"),
+    non_distinct_isins = c("non_distinct_isins", "non_distinct_isins_data"),
+    revenue = c("revenue", "revenue_data"),
+    sector_bridge = c("sector_bridge", "sector_bridge_data"),
+    security_financial = c("security_financial", "security_financial_data", "fin_data")
   )
 
 
