d45d9d93d4a2070acc546d588d7e19bfccc83cc0
CJ Yetman <cj@2degrees-investing.org>
2020-11-30 12:01:37 +0100

    test using an error collector

--- tests/testthat/test-validate_data_frame_with_more_than_0_rows.R
+++ tests/testthat/test-validate_data_frame_with_more_than_0_rows.R
@@ -21,3 +21,11 @@ test_that("validate_data_frame_with_more_than_0_rows() returns FALSE if the obje
     validate_data_frame_with_more_than_0_rows(data.frame())
   )
 })
+
+test_that("validate_data_frame_with_more_than_0_rows() can succesffully use an error collector", {
+  error_collector <- new_error_collector()
+  validate_data_frame_with_more_than_0_rows(data.frame(), error_collector = error_collector)
+  expect_false(
+    error_collector$isEmpty()
+  )
+})
