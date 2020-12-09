695fc26943bc2b21f2aad658a95070e54d0ed8f3
Mauro Lepore <maurolepore@gmail.com>
2020-12-09 12:23:11 -0300

    Handle new argument

--- web_tool_script_3.R
+++ web_tool_script_3.R
@@ -188,7 +188,7 @@ twodi_sectors = sector_list
 repo_path = template_path
 file_name = "template.Rmd"
 
-create_interactive_report(
+arguments <- list(
   repo_path = template_path,
   template_dir = template_dir,
   output_dir = output_dir,
@@ -234,10 +234,15 @@ create_interactive_report(
   ipr_results_stress_test = ipr_results_stress_test,
   display_currency = display_currency,
   currency_exchange_value = currency_exchange_value,
-  header_dictionary = header_dictionary,
- sector_order = sector_order()
+  header_dictionary = header_dictionary
 )
 
+has_arg <- utils::hasName(formals(create_executive_summary), "sector_order")
+if (has_arg) {
+  arg <- list(sector_order = sector_order())
+  arguments <- append(arguments, arg)
+}
+purrr::invoke(create_interactive_report, arguments)
 
 create_executive_summary(
   file_name = "template.Rmd",
