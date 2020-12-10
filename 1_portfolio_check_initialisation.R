d2df647fe07c9f1a918ca13aa8b464f04ad821ee
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-12-10 14:38:35 +0100

    initialize project

--- 1_portfolio_check_initialisation.R
+++ 1_portfolio_check_initialisation.R
@@ -25,15 +25,15 @@ source("0_portfolio_input_check_functions.R")
 source("0_global_functions.R")
 source("0_sda_approach.R")
 
-project_name <- "BondsTest"
-twodii_internal <- TRUE
+project_name <- "DFS_Project"
+twodii_internal <- FALSE
 # TRUE or FALSE: TRUE means that the code is running on a 2dii laptop with dropbox connection
 
 #####################################################################
 ### ONLY FOR EXTERNAL PROJECTS (twodii_internal <- FALSE):
 # Variables must exist for internal projects
-project_location_ext <- "C:/Users/clare/Desktop/ExternalTest"
-data_location_ext <- "C:/Users/clare/Desktop/Git/pacta-data/2019Q4"
+project_location_ext <- "~/Desktop/DFS"
+data_location_ext <- "~/Documents/github/2DegreesInvesting/pacta-data/2019Q4"
 #####################################################################
 
 create_project_folder(project_name, twodii_internal, project_location_ext)
