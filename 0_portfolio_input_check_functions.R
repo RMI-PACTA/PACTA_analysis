4fbcb9ddec4f1021ad7069c7da79e1a3ce608d24
Clare2D <clare@2degrees-investing.org>
2021-03-26 17:44:48 +0100

    check if column exists

--- 0_portfolio_input_check_functions.R
+++ 0_portfolio_input_check_functions.R
@@ -82,13 +82,17 @@ clean_portfolio_col_types <- function(portfolio, grouping_variables) {
     ),
     file_path = log_path)
   }
-  if (is.numeric(portfolio$number_of_shares) == FALSE) {
-    write_log(msg = paste0(
-      "Wrong variable class for number_of_shares Should be numeric, but is ",
-      class(portfolio$number_of_shares),
-      ". This can introduce errors in further calculations!"
-    ),
-    file_path = log_path)
+  if("number_of_shares" %in% colnames(portfolio)){
+    if (is.numeric(portfolio$number_of_shares) == FALSE) {
+      write_log(msg = paste0(
+        "Wrong variable class for number_of_shares Should be numeric, but is ",
+        class(portfolio$number_of_shares),
+        ". This can introduce errors in further calculations!"
+      ),
+      file_path = log_path)
+    }
+    portfolio$number_of_shares <- as.numeric(portfolio$number_of_shares)
+
   }
   if (is.character(portfolio$currency) == FALSE) {
     write_log(msg = paste0(
@@ -107,7 +111,6 @@ clean_portfolio_col_types <- function(portfolio, grouping_variables) {
     file_path = log_path)
   }
 
-  portfolio$number_of_shares <- as.numeric(portfolio$number_of_shares)
   portfolio$market_value <- as.numeric(portfolio$market_value)
   portfolio$currency <- as.character(portfolio$currency)
 
