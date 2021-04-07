ee6180fe77860a1f19c701a2131c7df0aed4b238
Mauro Lepore <maurolepore@gmail.com>
2021-04-07 10:08:22 -0700

    Rename to write_csv_file()

--- R/write_csv_file.R
+++ R/write_csv_file.R
@@ -1,4 +1,4 @@
-#' Backport the argument `file` to readr < 1.4.0
+#' A version of readr::write_csv() where `file` falls back to `path` if needed
 #'
 #' @param file File or connection to write to. In readr < 1.4.0 the name of this
 #'   argument is instead `path`. This wrapper helps use `file` regardless the
@@ -6,7 +6,7 @@
 #' @inheritParams readr::write_csv
 #' @inheritDotParams readr::write_csv
 #' @export
-safe_write_csv <- function(x, file, ...) {
+write_csv_file <- function(x, file, ...) {
   if (utils::packageVersion("readr") >= "1.4.0") {
     readr::write_csv(x = x, file = file, ...)
   } else {
--- tests/testthat/test-write_csv_file.R
+++ tests/testthat/test-write_csv_file.R
@@ -3,5 +3,5 @@ test_that("with readr < 1.4.0 and named argument `file` throws no error", {
 
   x <- tibble::tibble(x = 1)
   file <- tempfile()
-  expect_error(safe_write_csv(x = x, file = file), NA)
+  expect_error(write_csv_file(x = x, file = file), NA)
 })
