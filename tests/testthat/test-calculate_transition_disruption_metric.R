ad11e68928fcddd78da4c79f6c01e05be9018b21
Mauro Lepore <maurolepore@gmail.com>
2021-10-05 17:15:54 -0600

    Style

--- tests/testthat/test-calculate_transition_disruption_metric.R
+++ tests/testthat/test-calculate_transition_disruption_metric.R
@@ -11,45 +11,32 @@ test_that("with bad `data` errors with informative message", {
 })
 
 test_that("outputs a tibble", {
-  out <- calculate_transition_disruption_metric(
-    fake_pacta_results(year = c(2020, 2025, 2030)),
-    2020
-  )
+  pacta_results <- fake_pacta_results(year = c(2020, 2025, 2030))
+  out <- calculate_transition_disruption_metric(pacta_results, 2020)
   expect_is(out, "tbl_df")
 })
 
 test_that("FIXME: outputs `NaN` in columns `tdm_tech` and `tdm_sec`", {
-  out <- calculate_transition_disruption_metric(
-    fake_pacta_results(year = c(2020, 2025, 2030)),
-    2020
-  )
+  pacta_results <- fake_pacta_results(year = c(2020, 2025, 2030))
+  out <- calculate_transition_disruption_metric(pacta_results, 2020)
   expect_true(is.nan(out$tdm_tech))
   expect_true(is.nan(out$tdm_sec))
 })
 
 test_that("outputs is ungrouped", {
-  out <- calculate_transition_disruption_metric(
-    fake_pacta_results(
-      year = c(2020, 2025, 2030)
-    ),
-    2020
-  )
+  pacta_results <- fake_pacta_results(year = c(2020, 2025, 2030))
+  out <- calculate_transition_disruption_metric(pacta_results, 2020)
   expect_false(dplyr::is_grouped_df(out))
 })
 
 test_that("with data lacking crucial columns errors with informative message", {
   expect_error_missing_names <- function(name) {
-    bad_pacta_results <- dplyr::rename(
-      fake_pacta_results(year = c(2020, 2025, 2030)),
-      bad = name
-    )
+    pacta_results <- fake_pacta_results(year = c(2020, 2025, 2030))
+    bad_pacta_results <- dplyr::rename(pacta_results, bad = name)
 
     expect_error(
       class = "missing_names",
-      calculate_transition_disruption_metric(
-        bad_pacta_results,
-        2020
-      )
+      calculate_transition_disruption_metric(bad_pacta_results, 2020)
     )
   }
 
@@ -63,14 +50,11 @@ test_that("with data lacking crucial columns errors with informative message", {
 })
 
 test_that("warns if input data has only `ownership_weight` values of allocation", {
+  pacta_results <- fake_pacta_results(
+    allocation = "ownership_weight", year = c(2020, 2025, 2030)
+  )
   expect_warning(
-    calculate_transition_disruption_metric(
-      fake_pacta_results(
-        allocation = "ownership_weight",
-        year = c(2020, 2025, 2030)
-      ),
-      2020
-    ),
-    class = "has_zero_rows"
+    class = "has_zero_rows",
+    calculate_transition_disruption_metric(pacta_results, 2020)
   )
 })
