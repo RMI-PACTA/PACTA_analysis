81979845e861e529a1f391c1c361ecde20c425a0
Mauro Lepore <maurolepore@gmail.com>
2021-11-18 09:18:27 -0600

    FIXME: Define global variables
    Here I silent the R CMD check note but it's best to try define
    the variables in place.

--- R/imports.R
+++ R/imports.R
@@ -2,3 +2,14 @@
 #' @importFrom rlang abort warn inform %||%
 #' @importFrom fs path dir_create dir_delete dir_ls
 NULL
+
+globalVariables(
+  c(
+    "get_portfolio_name",
+    "outputs_path",
+    "portfolio_name",
+    "portfolio_name_ref_all",
+    "set_web_parameters",
+    "working_location"
+  )
+)
