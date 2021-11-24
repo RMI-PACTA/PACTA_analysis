da9dd7d448f75e678098cc3353fad5a5c79424f6
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-11-24 10:51:14 +0100

    dont rely on global env

--- R/tdm_utility_functions.R
+++ R/tdm_utility_functions.R
@@ -6,15 +6,15 @@ analysis_inputs_with_tdm_scenarios <- function() {
   c("../pacta-data/2020Q4")
 }
 
-data_includes_tdm_scenarios <- function() {
+data_includes_tdm_scenarios <- function(analysis_inputs_path) {
   analysis_inputs_path %in% analysis_inputs_with_tdm_scenarios()
 }
 
-tdm_conditions_met <- function() {
-  project_code == "GENERAL" && data_includes_tdm_scenarios()
+tdm_conditions_met <- function(analysis_inputs_path) {
+  project_code == "GENERAL" && data_includes_tdm_scenarios(analysis_inputs_path)
 }
 
-determine_tdm_variables <- function() {
+determine_tdm_variables <- function(start_year) {
   list(
     t0 = start_year,
     delta_t1 = 5,
