a71d712ba410753cbf7565212a5e88c33deddbef
CJ Yetman <cj2dii@cjyetman.local>
2021-12-01 05:16:49 +0100

    use function to return build version if available

--- R/utils.R
+++ R/utils.R
@@ -156,3 +156,8 @@ mark_end <- function() {
 expect_no_message <- function(object, regexp = NA, ...) {
   testthat::expect_message(object = object, regexp = regexp, ...)
 }
+
+get_build_version <- function(env_var = "$build_version") {
+  build_version <- system2("echo", args = env_var, stdout = TRUE)
+  ifelse(nzchar(build_version), paste0("v", build_version), NA_character_)
+}
--- web_tool_script_1.R
+++ web_tool_script_1.R
@@ -1,10 +1,10 @@
-build_version <- system2("echo", args = "$build_version", stdout = TRUE)
-if (nchar(build_version) > 0) build_version <- paste0(" - running in Docker build v", build_version)
-
-cli::cli_h1(paste0("web_tool_script_1.R", build_version))
+cli::cli_h1("web_tool_script_1.R")
 
 devtools::load_all(quiet = TRUE)
 use_r_packages()
+
+build_version <- get_build_version()
+if (!is.na(build_version)) cli::cli_h1(paste("Docker build", build_version))
 
 source("0_portfolio_input_check_functions.R")
 source("0_global_functions.R")
