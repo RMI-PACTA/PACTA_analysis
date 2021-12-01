38eee726707313d251f0ee3a626029c458f9fc20
CJ Yetman <cj@2degrees-investing.org>
2021-12-01 17:09:52 +0100

    use `Sys.getenv()`

--- R/utils.R
+++ R/utils.R
@@ -157,7 +157,7 @@ expect_no_message <- function(object, regexp = NA, ...) {
   testthat::expect_message(object = object, regexp = regexp, ...)
 }
 
-get_build_version <- function(env_var = "$build_version") {
-  build_version <- system2("echo", args = env_var, stdout = TRUE)
+get_build_version <- function(env_var = "build_version") {
+  build_version <- Sys.getenv(env_var)
   ifelse(nzchar(build_version), paste0("v", build_version), NA_character_)
 }
