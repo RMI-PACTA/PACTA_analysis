1ba0db405b2a3597af1e7921fd250c84dc551fd6
CJ Yetman <cj2dii@cjyetman.local>
2021-12-13 13:13:34 +0100

    use `skip_if_R_CMD_check()`

--- tests/testthat/test-add_fin_data.R
+++ tests/testthat/test-add_fin_data.R
@@ -1,6 +1,5 @@
 test_that("`set_portfolio_parameters()` works as expected", {
-  skip_if(nzchar(Sys.getenv("R_CMD")), "In R CMD check")
-  # replace with skip_if_R_CMD_check() when #547 merges
+  skip_if_R_CMD_check()
 
   source(here::here("0_portfolio_input_check_functions.R"))
 
