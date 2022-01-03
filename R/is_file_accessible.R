3215a838033f9f7e79403499d56b233e648c8864
CJ Yetman <cj@2degrees-investing.org>
2022-01-03 20:02:14 +0100

    add `is_file_accessible()`

---
+++ R/is_file_accessible.R
@@ -0,0 +1,17 @@
+is_file_accessible <- function(filepaths) {
+  if (is.data.frame(filepaths) && identical(length(filepaths), 1L)) {
+    filepaths <- filepaths[[1L]]
+  }
+
+  filepaths <- as.character(filepaths)
+  filepaths[is.na(filepaths)] <- FALSE
+
+  filepaths <- fs::path_abs(fs::path_expand(filepaths))
+
+  file_is_file <- fs::is_file(filepaths)
+  file_exists <- fs::file_exists(filepaths)
+  file_is_readable <- fs::file_access(filepaths, mode = "read")
+  file_not_empty <- !fs::is_file_empty(filepaths)
+
+  unname(file_is_file & file_exists & file_is_readable & file_not_empty)
+}
---
+++ tests/testthat/test-is_file_accessible.R
@@ -0,0 +1,34 @@
+test_that("outputs logical vector", {
+  directory <- file.path(tempdir(), "directory")
+  dir.create(directory)
+
+  empty_file <- tempfile()
+  invisible(file.create(empty_file))
+
+  no_read_access <- tempfile()
+  writeLines("XXX", no_read_access)
+  Sys.chmod(no_read_access, mode = "222")
+
+  accessible_file <- tempfile()
+  writeLines("XXX", accessible_file)
+
+  files <- c(directory, empty_file, no_read_access, accessible_file)
+
+  expect_vector(is_file_accessible(files[1]), ptype = logical(), size = 1)
+  expect_vector(is_file_accessible(files[2]), ptype = logical(), size = 1)
+  expect_vector(is_file_accessible(files[3]), ptype = logical(), size = 1)
+  expect_vector(is_file_accessible(files[4]), ptype = logical(), size = 1)
+  expect_vector(is_file_accessible(files), ptype = logical(), size = 4)
+
+  files_df <- data.frame(file = files)
+  expect_vector(mutate(files_df, accessible = is_file_accessible(file))$accessible, ptype = logical(), size = 4)
+  expect_vector(is_file_accessible(files_df$file), ptype = logical(), size = 4)
+  expect_vector(is_file_accessible(files_df["file"]), ptype = logical(), size = 4)
+  expect_vector(is_file_accessible(files_df[1]), ptype = logical(), size = 4)
+  expect_vector(is_file_accessible(files_df[["file"]]), ptype = logical(), size = 4)
+  expect_vector(is_file_accessible(files_df[[1]]), ptype = logical(), size = 4)
+
+  expect_vector(is_file_accessible(1:2), ptype = logical(), size = 2)
+  expect_vector(is_file_accessible(c(TRUE, FALSE)), ptype = logical(), size = 2)
+  expect_vector(is_file_accessible(c(NA, NA)), ptype = logical(), size = 2)
+})
