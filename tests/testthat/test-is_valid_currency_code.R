f2ce2ac257fd72696738a83eb61208c392de11b6
CJ Yetman <cj@2degrees-investing.org>
2022-01-04 03:30:53 +0100

    namespace `dplyr::mutate()`

--- tests/testthat/test-is_valid_currency_code.R
+++ tests/testthat/test-is_valid_currency_code.R
@@ -5,7 +5,7 @@ test_that("outputs logical vector", {
   expect_vector(is_valid_currency_code(codes), ptype = logical(), size = 2)
 
   codes_df <- data.frame(code = codes)
-  expect_vector(mutate(codes_df, valid_code = is_valid_currency_code(code))$valid_code, ptype = logical(), size = 2)
+  expect_vector(dplyr::mutate(codes_df, valid_code = is_valid_currency_code(code))$valid_code, ptype = logical(), size = 2)
   expect_vector(is_valid_currency_code(codes_df$code), ptype = logical(), size = 2)
   expect_vector(is_valid_currency_code(codes_df["code"]), ptype = logical(), size = 2)
   expect_vector(is_valid_currency_code(codes_df[1]), ptype = logical(), size = 2)
