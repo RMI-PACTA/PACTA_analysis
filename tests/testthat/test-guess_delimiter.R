78d0c07dae282f2ca0c6af8fcf5b6178d0d9cd42
CJ Yetman <cj@2degrees-investing.org>
2022-01-04 03:00:22 +0100

    add `guess_delimiter()`

---
+++ tests/testthat/test-guess_delimiter.R
@@ -0,0 +1,39 @@
+test_that("outputs character vector", {
+  directory <- file.path(tempdir(), "directory")
+  dir.create(directory, showWarnings = FALSE)
+
+  empty_file <- tempfile()
+  invisible(file.create(empty_file))
+
+  no_read_access <- tempfile()
+  saveRDS("XXX", no_read_access)
+  Sys.chmod(no_read_access, mode = "222")
+
+  binary_file <- tempfile()
+  saveRDS("XXX", binary_file)
+
+  accessible_file <- tempfile()
+  writeLines("XXX", accessible_file)
+
+  files <- c(directory, empty_file, no_read_access, binary_file, accessible_file)
+
+  expect_vector(guess_delimiter(files[1]), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(files[2]), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(files[3]), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(files[4]), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(files[5]), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(files), ptype = character(), size = 5)
+
+  files_df <- data.frame(file = files)
+  expect_vector(mutate(files_df, encoding = guess_delimiter(file))$encoding, ptype = character(), size = 5)
+  expect_vector(guess_delimiter(files_df$file), ptype = character(), size = 5)
+  expect_vector(guess_delimiter(files_df["file"]), ptype = character(), size = 5)
+  expect_vector(guess_delimiter(files_df[1]), ptype = character(), size = 5)
+  expect_vector(guess_delimiter(files_df[["file"]]), ptype = character(), size = 5)
+  expect_vector(guess_delimiter(files_df[[1]]), ptype = character(), size = 5)
+
+  expect_vector(guess_delimiter(1:2), ptype = character(), size = 2)
+  expect_vector(guess_delimiter(c(TRUE, FALSE)), ptype = character(), size = 2)
+  expect_vector(guess_delimiter(NA), ptype = character(), size = 1)
+  expect_vector(guess_delimiter(c(NA, NA)), ptype = character(), size = 2)
+})
