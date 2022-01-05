d98b13a959fbb1c6320e946819ed3cde328a0dea
CJ Yetman <cj@2degrees-investing.org>
2022-01-05 09:32:56 +0100

    use `{withr}` to make temp files and directory in tests

--- tests/testthat/test-is_binary_file.R
+++ tests/testthat/test-is_binary_file.R
@@ -1,18 +1,17 @@
 test_that("outputs logical vector", {
-  directory <- file.path(tempdir(), "directory")
-  dir.create(directory)
+  directory <- withr::local_tempdir()
 
-  empty_file <- tempfile()
+  empty_file <- withr::local_tempfile()
   invisible(file.create(empty_file))
 
-  no_read_access <- tempfile()
+  no_read_access <- withr::local_tempfile()
   saveRDS("XXX", no_read_access)
   Sys.chmod(no_read_access, mode = "222")
 
-  nonbinary_file <- tempfile()
+  nonbinary_file <- withr::local_tempfile()
   writeLines("XXX", nonbinary_file)
 
-  accessible_file <- tempfile()
+  accessible_file <- withr::local_tempfile()
   saveRDS("XXX", accessible_file)
 
   files <- c(directory, empty_file, no_read_access, nonbinary_file, accessible_file)
