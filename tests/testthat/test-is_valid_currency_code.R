221a0a8cfd0590076a93b0f2360501dd1eeb8ee8
CJ Yetman <cj@2degrees-investing.org>
2022-01-05 22:47:00 +0100

    improve tests

--- tests/testthat/test-is_valid_currency_code.R
+++ tests/testthat/test-is_valid_currency_code.R
@@ -1,18 +1,36 @@
-test_that("outputs logical vector", {
+test_that("always outputs a logical vector", {
+  # invalid and valid codes
   codes <- c(invalid_code = "XXX", valid_code = "USD")
   expect_vector(is_valid_currency_code(codes[1L]), ptype = logical(), size = 1L)
   expect_vector(is_valid_currency_code(codes[2L]), ptype = logical(), size = 1L)
   expect_vector(is_valid_currency_code(codes), ptype = logical(), size = 2L)
 
+  # expected possible uses
   codes_df <- data.frame(code = codes)
-  expect_vector(dplyr::mutate(codes_df, valid_code = is_valid_currency_code(code))$valid_code, ptype = logical(), size = 2L)
-  expect_vector(is_valid_currency_code(codes_df$code), ptype = logical(), size = 2L)
-  expect_vector(is_valid_currency_code(codes_df["code"]), ptype = logical(), size = 2L)
-  expect_vector(is_valid_currency_code(codes_df[1L]), ptype = logical(), size = 2L)
-  expect_vector(is_valid_currency_code(codes_df[["code"]]), ptype = logical(), size = 2L)
-  expect_vector(is_valid_currency_code(codes_df[[1L]]), ptype = logical(), size = 2L)
 
+  out <- dplyr::mutate(codes_df, valid_code = is_valid_currency_code(code))$valid_code
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  out <- is_valid_currency_code(codes_df$code)
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  out <- is_valid_currency_code(codes_df["code"])
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  out <- is_valid_currency_code(codes_df[1L])
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  out <- is_valid_currency_code(codes_df[["code"]])
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  out <- is_valid_currency_code(codes_df[[1L]])
+  expect_vector(out, ptype = logical(), size = 2L)
+
+  # unexpected input types
+  expect_vector(is_valid_currency_code(1L), ptype = logical(), size = 1L)
   expect_vector(is_valid_currency_code(1L:2L), ptype = logical(), size = 2L)
+  expect_vector(is_valid_currency_code(TRUE), ptype = logical(), size = 1L)
+  expect_vector(is_valid_currency_code(FALSE), ptype = logical(), size = 1L)
   expect_vector(is_valid_currency_code(c(TRUE, FALSE)), ptype = logical(), size = 2L)
   expect_vector(is_valid_currency_code(NA), ptype = logical(), size = 1L)
   expect_vector(is_valid_currency_code(c(NA, NA)), ptype = logical(), size = 2L)
