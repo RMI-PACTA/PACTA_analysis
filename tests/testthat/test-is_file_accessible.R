9e314a95b15316a95002ca37dc58e287762786bf
CJ Yetman <cj@2degrees-investing.org>
2022-01-06 12:04:24 +0100

    only test with no read access on posix because cannot create unreadable test file

--- tests/testthat/test-is_file_accessible.R
+++ tests/testthat/test-is_file_accessible.R
@@ -7,42 +7,37 @@ test_that("always outputs a logical vector", {
   empty_file <- withr::local_tempfile()
   invisible(file.create(empty_file))
 
-  no_read_access <- withr::local_tempfile()
-  writeLines("XXX", no_read_access)
-  Sys.chmod(no_read_access, mode = "222")
-
   accessible_file <- withr::local_tempfile()
   writeLines("XXX", accessible_file)
 
-  files <- c(non_existant_file, directory, empty_file, no_read_access, accessible_file)
+  files <- c(non_existant_file, directory, empty_file, accessible_file)
 
   expect_vector(is_file_accessible(files[1L]), ptype = logical(), size = 1L)
   expect_vector(is_file_accessible(files[2L]), ptype = logical(), size = 1L)
   expect_vector(is_file_accessible(files[3L]), ptype = logical(), size = 1L)
   expect_vector(is_file_accessible(files[4L]), ptype = logical(), size = 1L)
-  expect_vector(is_file_accessible(files[5L]), ptype = logical(), size = 1L)
-  expect_vector(is_file_accessible(files), ptype = logical(), size = 5L)
+  expect_vector(is_file_accessible(files), ptype = logical(), size = 4L)
 
   # expected possible uses
   files_df <- data.frame(file = files)
 
   out <- dplyr::mutate(files_df, accessible = is_file_accessible(file))$accessible
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   out <- is_file_accessible(files_df$file)
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   out <- is_file_accessible(files_df["file"])
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   out <- is_file_accessible(files_df[1L])
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   out <- is_file_accessible(files_df[["file"]])
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   out <- is_file_accessible(files_df[[1L]])
-  expect_vector(out, ptype = logical(), size = 5L)
+  expect_vector(out, ptype = logical(), size = 4L)
 
   # unexpected input types
   expect_vector(is_file_accessible(1L), ptype = logical(), size = 1L)
@@ -63,20 +58,27 @@ test_that("returns expected values", {
   empty_file <- withr::local_tempfile()
   invisible(file.create(empty_file))
 
-  no_read_access <- withr::local_tempfile()
-  writeLines("XXX", no_read_access)
-  Sys.chmod(no_read_access, mode = "222")
-
   accessible_file <- withr::local_tempfile()
   writeLines("XXX", accessible_file)
 
-  files <- c(non_existant_file, directory, empty_file, no_read_access, accessible_file)
+  files <- c(non_existant_file, directory, empty_file, accessible_file)
 
-  expect_identical(is_file_accessible(files), c(FALSE, FALSE, FALSE, FALSE, TRUE))
+  expect_identical(is_file_accessible(files), c(FALSE, FALSE, FALSE, TRUE))
 
   # unexpected input types
   expect_identical(is_file_accessible(NA), FALSE)
   expect_identical(is_file_accessible(NA_character_), FALSE)
   expect_identical(is_file_accessible(c(TRUE, FALSE)), c(FALSE, FALSE))
   expect_identical(is_file_accessible(1L:2L), c(FALSE, FALSE))
+})
+
+test_that("works as expected on a file that does not have read permission", {
+  skip_on_os(c("windows", "solaris"))
+
+  no_read_access <- withr::local_tempfile()
+  writeLines("XXX", no_read_access)
+  Sys.chmod(no_read_access, mode = "222")
+
+  expect_vector(is_file_accessible(no_read_access), ptype = logical(), size = 1L)
+  expect_identical(is_file_accessible(no_read_access), FALSE)
 })
