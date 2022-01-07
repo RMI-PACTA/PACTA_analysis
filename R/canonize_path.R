cf3af3e7566959b9d902a514781d57332975b57d
CJ Yetman <cj@2degrees-investing.org>
2022-01-07 15:53:57 +0100

    use descriptive object name

--- R/canonize_path.R
+++ R/canonize_path.R
@@ -1,11 +1,11 @@
 canonize_path <- function(path = ".") {
   vapply(
     X = path,
-    FUN = function(x) {
-      if (!is.character(x)) {
+    FUN = function(single_path) {
+      if (!is.character(single_path)) {
         return(NA_character_)
       }
-      fs::path_abs(fs::path_expand(enc2utf8(x)))
+      fs::path_abs(fs::path_expand(enc2utf8(single_path)))
     },
     FUN.VALUE = character(1L),
     USE.NAMES = FALSE
