95273d0f9fd6b3f303ae6600dd5e74377d363109
CJ Yetman <cj@2degrees-investing.org>
2022-01-12 12:43:43 +0100

    add `has_newline_at_end()`

---
+++ R/has_newline_at_end.R
@@ -0,0 +1,14 @@
+has_newline_at_end <- function(filepaths) {
+  filepaths <- simplify_if_one_col_df(filepaths)
+  stopifnot("`filepaths` must be a character vector" = typeof(filepaths) == "character")
+  filepaths <- canonize_path(filepaths)
+
+  vapply(
+    X = filepaths,
+    FUN = function(x) {
+      attr(R.utils::countLines.default(x), "lastLineHasNewline")
+    },
+    FUN.VALUE = logical(1),
+    USE.NAMES = FALSE
+  )
+}
