2f9252f3e9892e83567fbdd838656ca372e96710
CJ Yetman <cyetman@rmi.org>
2022-07-28 13:43:12 +0200

    use `company_id` as the matching column

--- 0_portfolio_test.R
+++ 0_portfolio_test.R
@@ -234,9 +234,10 @@ aggregate_map_data <- function(portfolio) {
 calculate_weights <- function(portfolio, portfolio_type, grouping_variables) {
   port_sub <- portfolio %>%
     select(
-      all_of(grouping_variables), holding_id, id, id_name, company_name, value_usd, number_of_shares,
+      all_of(grouping_variables), holding_id, id = company_id, id_name = "company_id", company_name, value_usd, number_of_shares,
       current_shares_outstanding_all_classes, financial_sector, has_ald_in_fin_sector
-    )
+    ) %>%
+    mutate(id = as.character(id))
 
   port_sub <- calculate_port_weight(port_sub, grouping_variables)
 
